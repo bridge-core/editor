36618350c2ed691cbd5c0dfb61f960543e4ea8a0
solvedDev <solvedDev@gmail.com>
2020-11-11 20:00:44 +0100

    add comments

--- src/appCycle/FileType.ts
+++ src/appCycle/FileType.ts
@@ -1,6 +1,9 @@
 import minimatch from 'minimatch'
 import json5 from 'json5'
 
+/**
+ * Describes the structure of a file definition
+ */
 interface IFileType {
 	id: string
 	matcher: string | string[]
@@ -8,12 +11,18 @@ interface IFileType {
 	packSpider: string
 }
 
+/**
+ * Used for return type of FileType.getMonacoSchemaArray() function
+ */
 interface IMonacoSchemaArrayEntry {
 	fileMatch?: string[]
 	uri: string
 	schema?: any
 }
 
+/**
+ * Utilities around bridge.'s file definitions
+ */
 export namespace FileType {
 	let fileTypes: IFileType[] = []
 
@@ -24,6 +33,10 @@ export namespace FileType {
 		fileTypes = json5.parse(jsonString) as IFileType[]
 	}
 
+	/**
+	 * Get the file definition data for the given file path
+	 * @param filePath file path to fetch file definition for
+	 */
 	export function get(filePath: string) {
 		for (const fileType of fileTypes) {
 			if (
@@ -37,10 +50,17 @@ export namespace FileType {
 		}
 	}
 
+	/**
+	 * Get the file type/file definition id for the provided file path
+	 * @param filePath file path to get the file type of
+	 */
 	export function getId(filePath: string) {
 		return get(filePath)?.id ?? 'unknown'
 	}
 
+	/**
+	 * Get a JSON schema array that can be used to set Monaco's JSON defaults
+	 */
 	export function getMonacoSchemaArray(): IMonacoSchemaArrayEntry[] {
 		return fileTypes
 			.map(({ matcher, schema }) => ({
