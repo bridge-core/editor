f266ccc4127985fb515e8af31bae3d444f74ebae
solvedDev <solvedDev@gmail.com>
2021-02-09 16:15:40 +0100

    feat: edit toolbar

--- src/components/Toolbar/setupDefaults.ts
+++ src/components/Toolbar/setupDefaults.ts
@@ -1,10 +1,12 @@
 import { App } from '@/App'
+import { setupEditCategory } from './Category/edit'
 import { setupFileCategory } from './Category/file'
 import { setupHelpCategory } from './Category/help'
 import { setupToolsCategory } from './Category/tools'
 
 export async function setupDefaultMenus(app: App) {
 	setupFileCategory(app)
+	setupEditCategory(app)
 	setupToolsCategory(app)
 	setupHelpCategory(app)
 }
