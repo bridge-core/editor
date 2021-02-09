35cf2a7d3355f3728c542902003f44c14a9c2eb6
solvedDev <solvedDev@gmail.com>
2021-02-09 14:22:45 +0100

    feat: draggable tabs

--- src/components/TabSystem/TabBar.vue
+++ src/components/TabSystem/TabBar.vue
@@ -1,6 +1,8 @@
 <template>
-	<div
+	<Draggable
 		v-if="tabSystem && tabSystem.tabs.length > 0"
+		v-model="tabSystem.tabs"
+		:group="{ name: 'tabSystemTabs' }"
 		:style="
 			`display: flex; overflow-x: scroll; white-space: nowrap; width: 100%; height: 48px;`
 		"
@@ -13,17 +15,19 @@
 			@click.middle.native="tab.close()"
 			@contextmenu.native="onContextMenu($event, i)"
 		/>
-	</div>
+	</Draggable>
 </template>
 
 <script>
 import { TabSystemMixin } from '../Mixins/TabSystem'
 import TabSystemTab from './Tab.vue'
+import Draggable from 'vuedraggable'
 
 export default {
 	mixins: [TabSystemMixin],
 	components: {
 		TabSystemTab,
+		Draggable,
 	},
 }
 </script>
