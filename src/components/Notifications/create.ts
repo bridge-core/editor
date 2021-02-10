d5176edc781f98ce691f85aad048aa75c9f48079
solvedDev <solvedDev@gmail.com>
2021-02-10 12:02:05 +0100

    upd: add class based notification API

--- src/components/Notifications/create.ts
+++ src/components/Notifications/create.ts
@@ -2,50 +2,14 @@ import { NotificationStore } from './state'
 import { v4 as uuid } from 'uuid'
 import Vue from 'vue'
 import { IDisposable } from '@/types/disposable'
-
-export interface INotification {
-	icon?: string
-	message?: string
-	color?: string
-	textColor?: string
-	disposeOnMiddleClick?: boolean
-
-	onClick?: () => void
-	onMiddleClick?: () => void
-}
+import { INotificationConfig, Notification } from './Notification'
 
 /**
  * Creates a new notification
  * @param config
  */
-export function createNotification(config: INotification): IDisposable {
-	if (!config.onClick)
-		config.onClick = () => {
-			//
-		}
-	const notificationUUID = uuid()
-	Vue.set(NotificationStore, notificationUUID, {
-		...config,
-		onMiddleClick: () => {
-			config.onMiddleClick?.()
-			if (config.disposeOnMiddleClick)
-				Vue.delete(NotificationStore, notificationUUID)
-		},
-	})
-	// @ts-expect-error
-	if (typeof navigator.setAppBadge === 'function')
-		// @ts-expect-error
-		navigator.setAppBadge(Object.keys(NotificationStore).length)
-
-	return {
-		dispose: () => {
-			Vue.delete(NotificationStore, notificationUUID)
-			// @ts-expect-error
-			if (typeof navigator.setAppBadge === 'function')
-				// @ts-expect-error
-				navigator.setAppBadge(Object.keys(NotificationStore).length)
-		},
-	}
+export function createNotification(config: INotificationConfig) {
+	return new Notification(config)
 }
 
 export function clearAllNotifications() {
