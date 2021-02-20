b77dd0ae0fc717213cf419447ca85ac066ef98b1
solvedDev <solvedDev@gmail.com>
2021-02-20 10:57:38 +0100

    feat: initial gametest declarations

---
+++ data/types/gametest.d.ts
@@ -0,0 +1,40 @@
+declare module 'GameTest' {
+	export function register(
+		namespace: string,
+		identifier: string,
+		func: (test: Test) => void
+	): TestRunner
+
+	class TestRunner {
+		maxTicks(ticks: number): TestRunner
+		batch(time: 'night'): TestRunner
+	}
+
+	class Test {
+		runAfterDelay(ticks: number, func: (test: Test) => void): void
+		succeed(): void
+		succeedWhen(func: (test: Test) => void): void
+		succeedWhenActorPresent(
+			id: string,
+			x: number,
+			y: number,
+			z: number
+		): void
+		succeedWhenBlockPresent(
+			id: string,
+			x: number,
+			y: number,
+			z: number
+		): void
+
+		failIf(func: (test: Test) => void): void
+
+		assertActorPresent(id: string, x: number, y: number, z: number): void
+		assertActorNotPresent(id: string, x: number, y: number, z: number): void
+		assertBlockPresent(id: string, x: number, y: number, z: number): void
+
+		spawn(id: string, x: number, y: number, z: number): void
+		setBlock(id: string, x: number, y: number, z: number): void
+		pressButton(x: number, y: number, z: number): void
+	}
+}
