711e5e8fab01e1d6c3163d166d66c1dc1db19582
solvedDev <solvedDev@gmail.com>
2021-02-21 22:25:05 +0100

    fix: creation of skins.json file upon creating skin pack

--- src/components/Projects/CreateProject/Packs/SP.ts
+++ src/components/Projects/CreateProject/Packs/SP.ts
@@ -2,6 +2,7 @@ import { CreatePack } from './Pack'
 import { CreateManifest } from '../Files/Manifest'
 import { CreateLang } from '../Files/Lang'
 import { CreatePackIcon } from '../Files/PackIcon'
+import { CreateSkins } from '../Files/Skins'
 
 export class CreateSP extends CreatePack {
 	protected readonly packPath = 'SP'
@@ -9,5 +10,6 @@ export class CreateSP extends CreatePack {
 		new CreateManifest(this.packPath),
 		new CreateLang(this.packPath),
 		new CreatePackIcon(this.packPath),
+		new CreateSkins(),
 	]
 }
