daa3065c6067c0138d56b9fc5365a7c63d2c0a6a
solvedDev <solvedDev@gmail.com>
2021-02-24 12:27:35 +0100

    feat: new script template

---
+++ data/preset/simpleFile/script/preset.js
@@ -0,0 +1,16 @@
+module.exports = async ({ createFile, loadPresetFile, models }) => {
+	console.log(models)
+	const { LANGUAGE, FILE_NAME, SCRIPT_TYPE } = models
+	const file =
+		SCRIPT_TYPE === 'Client'
+			? await loadPresetFile('scriptClient.js')
+			: await loadPresetFile('scriptServer.js')
+
+	await createFile(
+		`BP/scripts/${SCRIPT_TYPE.toLowerCase()}/${FILE_NAME}.${
+			LANGUAGE === 'JavaScript' ? 'js' : 'ts'
+		}`,
+		file
+	)
+	console.log('DONE')
+}
