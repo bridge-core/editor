65a630883d4c0bbcfe3d8488f97213da7d7879a2
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-03-03 23:46:16 +0100

    Update optionalImage.js

--- data/presetScript/optionalImage.js
+++ data/presetScript/optionalImage.js
@@ -1,5 +1,5 @@
 module.exports = async ({ createFile, loadPresetFile, models }) => {
-	const { IMAGE, IDENTIFIER, IMAGE_SAVE_PATH, DEFAULT_IMAGE } = models
+	let { IMAGE, IDENTIFIER, IMAGE_SAVE_PATH, DEFAULT_IMAGE } = models
 	if (!IMAGE) IMAGE = await loadPresetFile(DEFAULT_IMAGE)
 
 	await createFile(`${IMAGE_SAVE_PATH}${IDENTIFIER}.png`, IMAGE)
