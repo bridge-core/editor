fa390ad5856edce43193a662b5173b5e9116b847
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-03-03 23:46:27 +0100

    Update optionalModel.js

--- data/presetScript/optionalModel.js
+++ data/presetScript/optionalModel.js
@@ -1,5 +1,5 @@
 module.exports = async ({ createFile, loadPresetFile, models }) => {
-    const { MODEL, IDENTIFIER, MODEL_SAVE_PATH, DEFAULT_MODEL } = models
+    let { MODEL, IDENTIFIER, MODEL_SAVE_PATH, DEFAULT_MODEL } = models
     if(!MODEL) MODEL = await loadPresetFile(DEFAULT_MODEL)
 
     await createFile(`${MODEL_SAVE_PATH}${IDENTIFIER}.json`, MODEL)
