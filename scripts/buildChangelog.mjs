abf271e258a89d553bfd85d203b51dc3c8642d41
solvedDev <solvedDev@gmail.com>
2021-03-05 12:51:26 +0100

    upd: console.log for buildChangelog script (debugging)

--- scripts/buildChangelog.mjs
+++ scripts/buildChangelog.mjs
@@ -6,8 +6,9 @@ export async function buildChangelog() {
 	const html = await fetch(
 		'https://api.github.com/repos/bridge-core/editor/releases'
 	)
-		.then(response => response.json())
-		.then(data => new MarkdownIt().render(data[0].body))
+		.then((response) => response.json())
+		.then((data) => (console.log(data), data))
+		.then((data) => new MarkdownIt().render(data[0].body))
 
 	await fs.writeFile('public/changelog.html', html)
 }
