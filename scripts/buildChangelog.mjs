7792c674795556b012b7103bf5c418e35356f476
Pascal <pascal.de.heij12@gmail.com>
2021-03-07 20:56:52 +0100

    fix: changelog generation

--- scripts/buildChangelog.mjs
+++ scripts/buildChangelog.mjs
@@ -3,10 +3,23 @@ import { promises as fs } from 'fs'
 import fetch from 'node-fetch'
 
 export async function buildChangelog() {
+	let headers
+	const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+	if (GITHUB_TOKEN !== undefined) {
+		headers = {
+			Authorization: `token ${GITHUB_TOKEN}`,
+		}
+	}
+
 	const html = await fetch(
-		'https://api.github.com/repos/bridge-core/editor/releases'
+		'https://api.github.com/repos/bridge-core/editor/releases',
+		{
+			headers: headers,
+		},
 	)
 		.then((response) => response.json())
+		//TODO Remove console.log if successful for next release
 		.then((data) => (console.log(data), data))
 		.then((data) => new MarkdownIt().render(data[0].body))
 
