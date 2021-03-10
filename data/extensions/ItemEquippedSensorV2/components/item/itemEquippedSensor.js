3f18b6d47942c375fbcd6ac2fe02d09d6be248be
solvedDev <solvedDev@gmail.com>
2021-03-10 16:37:10 +0100

    upd: more unique item tag name

--- data/extensions/ItemEquippedSensorV2/components/item/itemEquippedSensor.js
+++ data/extensions/ItemEquippedSensorV2/components/item/itemEquippedSensor.js
@@ -9,52 +9,57 @@ export default defineComponent(({ name, template, schema }) => {
 						on_equip: {
 							type: 'array',
 							items: {
-								$ref: '/data/packages/schema/general/reference/animationEvent.json'
-							}
+								$ref:
+									'/data/packages/schema/general/reference/animationEvent.json',
+							},
 						},
 						on_unequip: {
 							type: 'array',
 							items: {
-								$ref: '/data/packages/schema/general/reference/animationEvent.json'
-							}
+								$ref:
+									'/data/packages/schema/general/reference/animationEvent.json',
+							},
 						},
 						slot: {
-							$ref: '/data/packages/schema/general/slotType.json'
-						}
-					}
-				}
-			}
-		}
+							$ref: '/data/packages/schema/general/slotType.json',
+						},
+					},
+				},
+			},
+		},
 	})
 
-	template(({ sensors }, { create, animationController }) => {
+	template(({ sensors }, { create, animationController, identifier }) => {
 		sensors.forEach((sensor, i) => {
-			const tag = `bridge:item_detection_tag_${i}`
+			const tag = `bridge:${identifier.split(':').pop()}_sensor_${i}`
 			const query = `query.equipped_item_any_tag('${sensor.slot}', '${tag}')`
 
-			create({
-				[`tag:${tag}`]: {}
-			}, 'minecraft:item/components')
+			create(
+				{
+					[`tag:${tag}`]: {},
+				},
+				'minecraft:item/components'
+			)
 			animationController({
 				default_state: 'not_equipped',
 				states: {
 					not_equipped: {
 						transitions: [
 							{
-								is_equipped: query
-							}
-						]
+								is_equipped: query,
+							},
+						],
 					},
 					is_equipped: {
 						transitions: [
 							{
-								not_equipped: `!${query}`
-							}
+								not_equipped: `!${query}`,
+							},
 						],
 						on_entry: sensor.on_equip,
 						on_exit: sensor.on_unequip,
-					}
-				}
+					},
+				},
 			})
 		})
 	})
