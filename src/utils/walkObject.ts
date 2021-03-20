17df42a3fbccdaf957bba2531392a5ca2e72b334
solvedDev <solvedDev@gmail.com>
2021-03-20 13:55:36 +0100

    feat: support '**' in walkObject calls

--- src/utils/walkObject.ts
+++ src/utils/walkObject.ts
@@ -1,3 +1,5 @@
+import { isMatch } from 'micromatch'
+
 export function walkObject(
 	path: string,
 	obj: any,
@@ -12,12 +14,26 @@ function _walkObject(
 	current: any,
 	onReach: (data: any) => void
 ) {
+	console.log(keys.join('/'))
 	if (current === undefined) return
 	if (keys.length === 0) return onReach(current)
 	if (typeof current !== 'object') return // Needs to be last because we want to make sure onReach gets called if possible
 
 	const key = keys.shift()!
-	if (key.startsWith('*')) {
+	if (key === '**') {
+		while (keys.length > 0 && keys[0] === '*') {
+			keys.shift()
+		}
+
+		const matcher = ['**', ...keys].join('/')
+		const collectedPaths = new Set<string>()
+		collectAllPaths('', current, collectedPaths)
+		for (const path of collectedPaths) {
+			if (isMatch(path, matcher)) {
+				_walkObject(path.split('/'), current, onReach)
+			}
+		}
+	} else if (key.startsWith('*')) {
 		let filterRegExp: RegExp | undefined = undefined
 		if (key.length >= 1)
 			filterRegExp = new RegExp(key.match(/(\*{)(.+)(})/)?.[2] ?? '.*')
@@ -27,6 +43,20 @@ function _walkObject(
 				_walkObject([...keys], current[key], onReach)
 		}
 	} else _walkObject(keys, current[key], onReach)
+}
+
+function collectAllPaths(
+	currPath: string,
+	current: any,
+	allPaths: Set<string>
+) {
+	for (const key in current) {
+		allPaths.add(`${currPath}${key}`)
+
+		if (typeof current[key] === 'object') {
+			collectAllPaths(`${currPath}${key}/`, current[key], allPaths)
+		}
+	}
 }
 
 export function setObjectAt<T = any>(
