effaccdc00969557b08fa40f7c36223f5d76b9a9
solvedDev <solvedDev@gmail.com>
2021-03-23 11:06:19 +0100

    upd: add comments to MoLang compiler plugin

--- src/components/Compiler/Worker/Plugins/MoLang/Plugin.ts
+++ src/components/Compiler/Worker/Plugins/MoLang/Plugin.ts
@@ -6,6 +6,7 @@ import json5 from 'json5'
 export const MoLangPlugin: TCompilerPluginFactory<{
 	include: Record<string, string[]>
 }> = ({ fileSystem, options: { include = {} } = {} }) => {
+	//Custom MoLang parser from https://github.com/bridge-core/MoLang
 	const customMoLang = new CustomMoLang({})
 	const isMoLangFile = (filePath: string | null) =>
 		filePath?.endsWith('.molang')
@@ -16,6 +17,7 @@ export const MoLangPlugin: TCompilerPluginFactory<{
 
 	return {
 		async buildStart() {
+			// Load default MoLang locations and merge them with user defined locations
 			include = Object.assign(
 				await fileSystem.readJSON(
 					'data/packages/location/validMoLang.json'
@@ -24,24 +26,29 @@ export const MoLangPlugin: TCompilerPluginFactory<{
 			)
 		},
 		transformPath(filePath) {
+			// MoLang files should get omitted from output
 			if (isMoLangFile(filePath)) return null
 		},
 		async read(filePath, fileHandle) {
 			if (isMoLangFile(filePath) && fileHandle) {
+				// Load MoLang files as text
 				const file = await fileHandle.getFile()
 				return await file.text()
 			} else if (loadMoLangFrom(filePath) && fileHandle) {
+				// Currently, MoLang in function files is not supported so we can just load files as JSON
 				const file = await fileHandle.getFile()
 				return json5.parse(await file.text())
 			}
 		},
 		async load(filePath, fileContent) {
 			if (isMoLangFile(filePath) && fileContent) {
+				// Load the custom MoLang functions
 				customMoLang.parse(fileContent)
 			}
 		},
 		async require(filePath) {
 			if (loadMoLangFrom(filePath)) {
+				// Register molang files as JSON file dependencies
 				return ['*/molang/**/*.molang']
 			}
 		},
@@ -50,9 +57,12 @@ export const MoLangPlugin: TCompilerPluginFactory<{
 			const includePaths = loadMoLangFrom(filePath)
 
 			if (includePaths && includePaths.length > 0) {
+				// For every MoLang location
 				includePaths.forEach((includePath) =>
+					// Search it inside of the JSON & transform it if it exists
 					setObjectAt<string>(includePath, fileContent, (molang) => {
 						if (typeof molang !== 'string') return molang
+						// We don't want to transform entity events & slash commands inside of animations/animation controllers
 						if (molang[0] === '/' || molang[0] === '@')
 							return molang
 
@@ -67,6 +77,7 @@ export const MoLangPlugin: TCompilerPluginFactory<{
 		},
 
 		finalizeBuild(filePath, fileContent) {
+			// Make sure JSON files are transformed back into a format that we can write to disk
 			if (loadMoLangFrom(filePath) && typeof fileContent !== 'string')
 				return JSON.stringify(fileContent, null, '\t')
 		},
