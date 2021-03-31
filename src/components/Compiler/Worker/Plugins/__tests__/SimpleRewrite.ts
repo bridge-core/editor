b131b25d851ab1d15a44b8e12a4ca1858eb5160f
solvedDev <solvedDev@gmail.com>
2021-03-31 18:58:52 +0200

    upd: add more tests for simpleRewrite plugin to ensure its functionality

--- src/components/Compiler/Worker/Plugins/__tests__/SimpleRewrite.ts
+++ src/components/Compiler/Worker/Plugins/__tests__/SimpleRewrite.ts
@@ -26,8 +26,35 @@ describe('ComMojangRewrite Compiler Plugin', () => {
 			'builds/dist/Bridge SP/textures/skin1.png'
 		)
 	})
+	it('should omit gametest files', () => {
+		expect(
+			rewrite.transformPath('BP/scripts/gametests/test.js')
+		).toBeUndefined()
+	})
 
 	const devRewrite = <TCompilerPlugin>SimpleRewrite(<any>{
+		options: { mode: 'dev' },
+		fileSystem,
+		compileFiles: async () => {},
+		getAliases: () => [],
+	})
+	it('should put BP into correct folder', () => {
+		expect(devRewrite.transformPath('BP/entities/test.json')).toEqual(
+			'builds/dev/Bridge BP/entities/test.json'
+		)
+	})
+	it('should put RP into correct folder', () => {
+		expect(devRewrite.transformPath('RP/entity/test.json')).toEqual(
+			'builds/dev/Bridge RP/entity/test.json'
+		)
+	})
+	it('should put SP into correct folder', () => {
+		expect(devRewrite.transformPath('SP/textures/skin1.png')).toEqual(
+			'builds/dev/Bridge SP/textures/skin1.png'
+		)
+	})
+
+	const comMojangDevRewrite = <TCompilerPlugin>SimpleRewrite(<any>{
 		options: { mode: 'dev' },
 		fileSystem,
 		compileFiles: async () => {},
@@ -36,18 +63,18 @@ describe('ComMojangRewrite Compiler Plugin', () => {
 	})
 
 	it('should put BP into correct folder in dev mode & with access to com.mojang folder', () => {
-		expect(devRewrite.transformPath('BP/entities/test.json')).toEqual(
-			'development_behavior_packs/Bridge BP/entities/test.json'
-		)
+		expect(
+			comMojangDevRewrite.transformPath('BP/entities/test.json')
+		).toEqual('development_behavior_packs/Bridge BP/entities/test.json')
 	})
 	it('should put RP into correct folder in dev mode & with access to com.mojang folder', () => {
-		expect(devRewrite.transformPath('RP/entity/test.json')).toEqual(
-			'development_resource_packs/Bridge RP/entity/test.json'
-		)
+		expect(
+			comMojangDevRewrite.transformPath('RP/entity/test.json')
+		).toEqual('development_resource_packs/Bridge RP/entity/test.json')
 	})
 	it('should put SP into correct folder in dev mode & with access to com.mojang folder', () => {
-		expect(devRewrite.transformPath('SP/textures/skin1.png')).toEqual(
-			'skin_packs/Bridge SP/textures/skin1.png'
-		)
+		expect(
+			comMojangDevRewrite.transformPath('SP/textures/skin1.png')
+		).toEqual('skin_packs/Bridge SP/textures/skin1.png')
 	})
 })
