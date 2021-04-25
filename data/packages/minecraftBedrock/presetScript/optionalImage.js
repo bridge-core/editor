6d6699e1e38f7bd2bcf5aa46672b07bf3752ece5
Joelant05 <Joelant05@users.noreply.github.com>
2021-04-25 11:16:00 +0100

    upd: implement PRESET_PATHs

--- data/packages/minecraftBedrock/presetScript/optionalImage.js
+++ data/packages/minecraftBedrock/presetScript/optionalImage.js
@@ -1,6 +1,12 @@
 module.exports = async ({ createFile, loadPresetFile, models }) => {
-	let { IMAGE, IDENTIFIER, IMAGE_SAVE_PATH, DEFAULT_IMAGE } = models
+	let {
+		IMAGE,
+		IDENTIFIER,
+		IMAGE_SAVE_PATH,
+		DEFAULT_IMAGE,
+		PRESET_PATH,
+	} = models
 	if (!IMAGE) IMAGE = await loadPresetFile(DEFAULT_IMAGE)
 
-	await createFile(`${IMAGE_SAVE_PATH}${IDENTIFIER}.png`, IMAGE)
+	await createFile(`${IMAGE_SAVE_PATH}${PRESET_PATH}${IDENTIFIER}.png`, IMAGE)
 }
