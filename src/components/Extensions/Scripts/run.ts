24f9f5328c0983f2324d941e75cba86a712b8540
solvedDev <solvedDev@gmail.com>
2021-06-20 14:50:42 +0200

    upd(ESM imports): support "import * as X"

--- src/components/Extensions/Scripts/run.ts
+++ src/components/Extensions/Scripts/run.ts
@@ -32,16 +32,20 @@ export function createRunner(
 }
 
 export function transformScript(script: string) {
-	return `${script
-		.replace(/export default /g, 'module.exports = ')
-		// TODO: Support named exports
-		// .replace(/export (var|const|let|function|class) /g, (substr) => {
-		// 	return substr
-		// })
-		.replace(
-			/import\s+([a-z][a-z0-9]+|{[a-z\s][a-z0-9,\s]+})\s+from\s+'(.+)'/gi,
-			(_, imports, moduleName) => {
-				return `const ${imports} = await require('${moduleName}')`
-			}
-		)}`
+	return (
+		script
+			.replace(/export default /g, 'module.exports = ')
+			// TODO: Support named exports
+			// .replace(/export (var|const|let|function|class) /g, (substr) => {
+			// 	return substr
+			// })
+			.replace(
+				/import\s+(\* as [a-z][a-z0-9]*|[a-z][a-z0-9]+|{[a-z\s][a-z0-9,\s]*})\s+from\s+["'](.+)["']/gi,
+				(_, imports, moduleName) => {
+					if (imports.startsWith(`* as `))
+						imports = imports.replace('* as ', '')
+					return `const ${imports} = await require('${moduleName}')`
+				}
+			)
+	)
 }
