4619299dc5de2b7ee74435a49810c37aced0199e
solvedDev <solvedDev@gmail.com>
2021-07-05 14:17:59 +0200

    feat: support for loading v1's custom component auto-completions

--- src/components/Compiler/Worker/Plugins/CustomComponent/v1Compat.ts
+++ src/components/Compiler/Worker/Plugins/CustomComponent/v1Compat.ts
@@ -6,12 +6,16 @@ export const v1Compat = (module: any, fileType: string) => ({
 		if ((componentClass.type ?? 'entity') !== fileType) return
 
 		module.exports = ({ name, schema, template }: any) => {
+			const component = new componentClass()
+
 			name(componentClass.component_name)
-			schema({})
+			schema(
+				transformV1AutoCompletions(
+					Object.values(component.onPropose())[0]
+				)
+			)
 
 			template((componentArgs: any, { create }: any) => {
-				const component = new componentClass()
-
 				create(
 					component.onApply(componentArgs, 'components')[
 						`minecraft:${fileType}`
@@ -21,3 +25,52 @@ export const v1Compat = (module: any, fileType: string) => ({
 		}
 	},
 })
+
+function transformV1AutoCompletions(completions: any): any {
+	const v2Completions: any = {}
+
+	// v1 array schema compatibility
+	const keys = Object.keys(completions)
+	if (keys.length === 1 && keys[0].startsWith('$dynamic.list.')) {
+		return {
+			type: 'array',
+			items: transformV1AutoCompletions(Object.values(completions)[0]),
+		}
+	}
+
+	// v1 property compatibility
+	for (const [propertyName, value] of Object.entries(completions)) {
+		if (typeof value === 'string')
+			v2Completions[propertyName] = transformV1Value(value)
+		else if (Array.isArray(value))
+			v2Completions[propertyName] = { enum: value }
+	}
+
+	return { type: 'object', properties: v2Completions }
+}
+
+/**
+ * Takes a v1 string auto-completion reference like "$general.boolean"
+ * and transforms it to a standard JSON schema
+ */
+function transformV1Value(value: string) {
+	switch (value) {
+		case '$general.boolean':
+			return { type: 'boolean' }
+		case '$general.number':
+			return { type: 'integer' }
+		case '$general.decimal':
+			return { type: 'number' }
+		default:
+			return {
+				type: [
+					'number',
+					'integer',
+					'string',
+					'boolean',
+					'object',
+					'array',
+				],
+			}
+	}
+}
