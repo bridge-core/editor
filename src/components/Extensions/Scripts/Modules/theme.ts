c1d746b09ca9d35c572e9b1bf808aca70bf864d0
solvedDev <solvedDev@gmail.com>
2021-07-09 15:13:54 +0200

    feat: @bridge/theme module

---
+++ src/components/Extensions/Scripts/Modules/theme.ts
@@ -0,0 +1,19 @@
+import { App } from '/@/App'
+import { IModuleConfig } from '../types'
+import { TColorName } from '../../Themes/ThemeManager'
+
+export const ThemeModule = async ({ disposables }: IModuleConfig) => {
+	const app = await App.getApp()
+	const themeManager = app.themeManager
+
+	return {
+		onChange: (func: (mode: 'dark' | 'light') => void) => {
+			const disposable = themeManager.on(func)
+			disposables.push(disposable)
+			return disposable
+		},
+		getColor(name: TColorName) {
+			return themeManager.getColor(name)
+		},
+	}
+}
