56e59ecab8fdb36d30cef443c9a7dc7779211b30
Joelant05 <Joelant05@users.noreply.github.com>
2021-07-15 11:24:33 +0100

    fix: set identifier in imported geometry

--- data/packages/minecraftBedrock/presetScript/optionalModel.js
+++ data/packages/minecraftBedrock/presetScript/optionalModel.js
@@ -1,9 +1,22 @@
 module.exports = async ({ createFile, loadPresetFile, models }) => {
 	let { MODEL, IDENTIFIER, MODEL_SAVE_PATH, DEFAULT_MODEL } = models
 	if (!MODEL) MODEL = await loadPresetFile(DEFAULT_MODEL)
-	const DATA = await MODEL.text()
+	let DATA = await MODEL.text()
+	let NEW_DATA = ''
 
-	await createFile(`${MODEL_SAVE_PATH}${IDENTIFIER}.json`, DATA, {
+	try {
+		NEW_DATA = JSON.parse(DATA)
+		if (NEW_DATA['minecraft:geometry']?.[0]?.description?.identifier)
+			NEW_DATA[
+				'minecraft:geometry'
+			][0].description.identifier = `geometry.${IDENTIFIER}`
+
+		NEW_DATA = JSON.stringify(NEW_DATA, null, '\t')
+	} catch {
+		NEW_DATA = DATA
+	}
+
+	await createFile(`${MODEL_SAVE_PATH}${IDENTIFIER}.json`, NEW_DATA, {
 		inject: ['IDENTIFIER'],
 	})
 }
