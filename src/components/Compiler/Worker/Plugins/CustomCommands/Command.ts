acae8cfb42a539429156a46999ece3f0614b26cd
solvedDev <33347616+solvedDev@users.noreply.github.com>
2021-07-24 08:43:24 +0200

    fix: custom commands with simple schemas not appearing inside of auto-completions

--- src/components/Compiler/Worker/Plugins/CustomCommands/Command.ts
+++ src/components/Compiler/Worker/Plugins/CustomCommands/Command.ts
@@ -96,12 +96,15 @@ export class Command {
 	}
 
 	getSchema() {
-		if (!this.schema) return []
-		else if (Array.isArray(this.schema))
+		if (!this.schema) return [{ commandName: this.name }]
+		else if (Array.isArray(this.schema)) {
+			if (this.schema.length === 0) return [{ commandName: this.name }]
+
 			return this.schema.map((schema) => ({
 				commandName: this.name,
 				...schema,
 			}))
+		}
 
 		// If commandName is not set on the schema, set it to the current name of the command.
 		if (!this.schema.commandName) this.schema.commandName = this.name
