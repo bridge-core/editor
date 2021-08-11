6ee8971e5ff5440025f33b9b910ac98a73365543
solvedDev <solvedDev@gmail.com>
2021-08-11 18:02:45 +0200

    fix: don't write minified player.json file to disk

--- src/components/Projects/CreateProject/Files/BP/Player.ts
+++ src/components/Projects/CreateProject/Files/BP/Player.ts
@@ -6,11 +6,11 @@ export class CreatePlayer extends CreateFile {
 	async create(fs: FileSystem) {
 		const app = await App.getApp()
 		await app.dataLoader.fired
-		const defaultPlayer = await app.dataLoader.readFile(
+		const defaultPlayer = await app.dataLoader.readJSON(
 			'data/packages/minecraftBedrock/vanilla/player.json'
 		)
 
-		await fs.writeFile('BP/entities/player.json', defaultPlayer)
+		await fs.writeJSON('BP/entities/player.json', defaultPlayer, true)
 		await fs.writeFile('BP/loot_tables/empty.json', '{}')
 	}
 }
