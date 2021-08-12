8ddb8bf444fdf6f82647e00bfb1b168750e7b8e3
solvedDev <solvedDev@gmail.com>
2021-08-12 23:49:21 +0200

    upd: change app name of nightly builds

--- src/components/Projects/Title.ts
+++ src/components/Projects/Title.ts
@@ -1,15 +1,17 @@
 import { ref } from '@vue/composition-api'
+import { isNightly } from '/@/utils/app/isNightly'
 
+const appName = isNightly ? 'bridge. Nightly' : 'bridge. v2'
 export class Title {
 	protected titleTag: HTMLTitleElement
-	public current = ref('bridge. v2')
+	public current = ref(appName)
 
 	constructor() {
 		this.titleTag = document.head.getElementsByTagName('title')[0]
 	}
 
 	setProject(projectName: string) {
-		this.titleTag.innerText = `${projectName} - bridge. v2`
-		this.current.value = `${projectName} - bridge. v2`
+		this.titleTag.innerText = `${projectName} - ${appName}`
+		this.current.value = `${projectName} - ${appName}`
 	}
 }
