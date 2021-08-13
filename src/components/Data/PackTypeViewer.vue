590aad929d292c75f470983f811afbb9a71c58ef
solvedDev <solvedDev@gmail.com>
2021-08-13 22:45:15 +0200

    fix: packs not being selectable within create project window

--- src/components/Data/PackTypeViewer.vue
+++ src/components/Data/PackTypeViewer.vue
@@ -5,7 +5,7 @@
 			selected: selected,
 		}"
 		v-ripple="isSelectable"
-		@click.stop="$emit('click')"
+		@click.stop.native="$emit('click')"
 	>
 		<div class="d-flex">
 			<v-icon :color="packType.color" class="mr-2">
