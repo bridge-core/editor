a6b3424db8d6905766f2327d73f86458d56061dc
solvedDev <solvedDev@gmail.com>
2021-09-13 13:37:44 +0200

    fix: remove unused import

--- src/components/Extensions/Scripts/Modules/project.ts
+++ src/components/Extensions/Scripts/Modules/project.ts
@@ -5,7 +5,6 @@ import {
 	IExporter,
 } from '/@/components/Projects/Export/Extensions/Exporter'
 import { TPackTypeId } from '/@/components/Data/PackType'
-import { time } from 'console'
 
 export const ProjectModule = async ({
 	disposables,
