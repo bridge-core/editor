683601cedda60ded42177093f43ab6f94ca7e4d7
Jason <im@jasongardner.co>
2021-10-26 09:14:41 -0500

    upd: refactored TargaTab class (#205)
    - Added ability to toggle alpha transparency mask by triggering a SimpleAction
    - Removed `.targa` from file extension matches. (Minecraft does not look for .targa texture files.)

--- src/components/Editors/Image/TargaTab.ts
+++ src/components/Editors/Image/TargaTab.ts
@@ -1,20 +1,51 @@
+import TgaLoader from 'tga-js'
 import { ImageTab } from './ImageTab'
 import { AnyFileHandle } from '../../FileSystem/Types'
-import TgaLoader from 'tga-js'
+import { SimpleAction } from '../../Actions/SimpleAction'
 
 export class TargaTab extends ImageTab {
+	protected tga = new TgaLoader()
+	maskIsApplied: boolean = true
+
 	static is(fileHandle: AnyFileHandle) {
-		const fileName = fileHandle.name
-		return fileName.endsWith('.tga') || fileName.endsWith('.targa')
+		return fileHandle.name.endsWith('.tga')
+	}
+
+	async setup() {
+		await super.setup()
+
+		this.addAction(
+			new SimpleAction({
+				icon: 'mdi-square-opacity',
+				name: 'Show/Hide Alpha Mask',
+				onTrigger: async () => {
+					if (this.maskIsApplied) {
+						await this.applyUnmaskedImageUrl()
+						this.maskIsApplied = false
+						return
+					}
+
+					this.applyMaskedImageUrl()
+					this.maskIsApplied = true
+				},
+			})
+		)
 	}
 
 	async onActivate() {
-		const loader = new TgaLoader()
+		this.isLoading = true
+
 		const file = await this.fileHandle.getFile()
+		this.tga.load(new Uint8Array(await file.arrayBuffer()))
 
-		loader.load(new Uint8Array(await file.arrayBuffer()))
+		this.isLoading = false
 
-		this.dataUrl = loader.getDataURL('image/png')
+		if (this.maskIsApplied) {
+			this.applyMaskedImageUrl()
+			return
+		}
+
+		await this.applyUnmaskedImageUrl()
 	}
 
 	save() {
@@ -23,5 +54,51 @@ export class TargaTab extends ImageTab {
 
 	async saveAs() {
 		/// TODO: Save `this.dataUrl` value to user input
+	}
+
+	applyMaskedImageUrl() {
+		this.dataUrl = this.tga.getDataURL('image/png')
+	}
+
+	async applyUnmaskedImageUrl() {
+		/// Get ImageData from TGALoader
+		const { width, height, data } = this.tga.getImageData()
+
+		// @ts-ignore OffscreenCanvas API types not available in TypeScript
+		const offscreen = new OffscreenCanvas(width, height)
+
+		/// Create context to contain new image
+		const ctx = offscreen.getContext('2d')
+		const imageData = ctx.createImageData(width, height)
+
+		/// Rewrite ImageData
+		/// Copies RGB channels from original data
+		/// Clamps alpha channel to be fully opaque (white)
+		const len = data.length
+
+		for (let itr = 0; itr < len; itr += 4) {
+			imageData.data[itr] = data[itr]
+			imageData.data[itr + 1] = data[itr + 1]
+			imageData.data[itr + 2] = data[itr + 2]
+			imageData.data[itr + 3] = 255
+		}
+
+		ctx.putImageData(imageData, 0, 0)
+
+		const canvasBlob = await offscreen.convertToBlob({
+			type: 'image/png',
+		})
+
+		/// Convert OffscreenCanvas content to Blob, so it can be converted to base64
+		const reader = new FileReader()
+		reader.readAsDataURL(canvasBlob)
+
+		reader.onload = () => {
+			this.dataUrl = reader.result?.toString()
+		}
+
+		reader.onerror = () => {
+			throw Error('Failed reading OffscreenCanvas Blob')
+		}
 	}
 }
