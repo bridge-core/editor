bcc249191b44b87ae2eb2569b22b27cd65e35f53
7dev7urandom <7dev7urandom@users.noreply.github.com>
2021-11-29 19:56:26 +0700

    fix: syntax error in zhCN.ts

--- src/locales/zhCN.ts
+++ src/locales/zhCN.ts
@@ -27,8 +27,7 @@ export default {
 		clear: '清除',
 		reset: '重置',
 
-		confirmOverwriteFile:
-			'此操作会覆盖同名文件。你要继续吗？',
+		confirmOverwriteFile: '此操作会覆盖同名文件。你要继续吗？',
 		fileSystemPolyfill:
 			'由于你的浏览器原因，你需要将你的项目下载到本地来实际保存你的进度。如果你使用的是Chrome（除了Chrome 93或94）或Edge，就无需这么做了！',
 		successfulExport: {
@@ -41,17 +40,15 @@ export default {
 	packType: {
 		behaviorPack: {
 			name: '行为包',
-			description:
-				'创建新的游戏机制并改变Minecraft的行为方式',
+			description: '创建新的游戏机制并改变Minecraft的行为方式',
 		},
 		resourcePack: {
 			name: '资源包',
-			description: "用于改变Minecraft的外观和声音",
+			description: '用于改变Minecraft的外观和声音',
 		},
 		skinPack: {
 			name: '皮肤包',
-			description:
-				'提供玩家角色可以选择的全新样貌',
+			description: '提供玩家角色可以选择的全新样貌',
 		},
 		worldTemplate: {
 			name: '世界模板',
@@ -141,8 +138,7 @@ export default {
 		},
 		saveAs: {
 			name: '另存为',
-			description:
-				'以不同的名称保存当前打开的文件',
+			description: '以不同的名称保存当前打开的文件',
 		},
 		saveAll: {
 			name: '保存全部',
@@ -154,7 +150,7 @@ export default {
 		},
 		settings: {
 			name: '设置',
-			description: "打开bridge.的应用设置",
+			description: '打开bridge.的应用设置',
 		},
 		extensions: {
 			name: '扩展',
@@ -166,8 +162,7 @@ export default {
 		},
 		cut: {
 			name: '剪切',
-			description:
-				'将选定的文本复制到剪贴板并将其从原始上下文中删除',
+			description: '将选定的文本复制到剪贴板并将其从原始上下文中删除',
 		},
 		paste: {
 			name: '粘贴',
@@ -187,7 +182,7 @@ export default {
 		},
 		extensionAPI: {
 			name: '扩展API',
-			description: "阅读有关bridge.扩展API的更多信息",
+			description: '阅读有关bridge.扩展API的更多信息',
 		},
 		gettingStarted: {
 			name: '新手入门',
@@ -195,8 +190,7 @@ export default {
 		},
 		faq: {
 			name: '常见问题解答',
-			description:
-				'阅读有关使用bridge.开发附加包的常见问题解答',
+			description: '阅读有关使用bridge.开发附加包的常见问题解答',
 		},
 		reloadAutoCompletions: {
 			name: '重载自动补全',
@@ -237,13 +231,11 @@ export default {
 		pluginInstallLocation: {
 			global: {
 				name: '全局安装',
-				description:
-					'全局扩展在你的所有项目中都可以访问',
+				description: '全局扩展在你的所有项目中都可以访问',
 			},
 			local: {
 				name: '局部安装',
-				description:
-					'局部扩展只能它被添加到的项目内部可以访问',
+				description: '局部扩展只能它被添加到的项目内部可以访问',
 			},
 		},
 		toObject: {
@@ -360,18 +352,15 @@ export default {
 	experimentalGameplay: {
 		cavesAndCliffs: {
 			name: '洞穴与山崖',
-			description:
-				'为生物群系内的新一代山峰内容启用自动补全。',
+			description: '为生物群系内的新一代山峰内容启用自动补全。',
 		},
 		holidayCreatorFeatures: {
 			name: '假日创作者功能',
-			description:
-				'为数据驱动的物品和方块功能启用自动补全。',
+			description: '为数据驱动的物品和方块功能启用自动补全。',
 		},
 		creationOfCustomBiomes: {
 			name: '自定义生物群系的创建',
-			description:
-				'允许创建自定义生物群系、地物和地物规则。',
+			description: '允许创建自定义生物群系、地物和地物规则。',
 		},
 		additionalModdingCapabilities: {
 			name: '其他修改功能‌',
@@ -380,8 +369,7 @@ export default {
 		},
 		upcomingCreatorFeatures: {
 			name: '即将推出的创作者功能‌',
-			description:
-				'启用迷雾功能域文件的创建并提供实体属性的自动补全。',
+			description: '启用迷雾功能域文件的创建并提供实体属性的自动补全。',
 		},
 		enableGameTestFramework: {
 			name: '启用GameTest框架‌',
@@ -390,19 +378,17 @@ export default {
 		},
 		experimentalMolangFeatures: {
 			name: '实验Molang功能‌',
-			description:
-				'为实验性Molang查询启用自动补全。',
+			description: '为实验性Molang查询启用自动补全。',
 		},
 		educationEdition: {
 			name: '启用教育版',
-			description:
-				'启用教育版功能的自动补全，例如材料分解器配方。',
+			description: '启用教育版功能的自动补全，例如材料分解器配方。',
 		},
 	},
 	// Windows
 	windows: {
 		changelogWindow: {
-			title: "有何新内容？",
+			title: '有何新内容？',
 		},
 		openFile: {
 			title: '打开',
@@ -411,8 +397,7 @@ export default {
 		},
 		createProject: {
 			welcome: '欢迎来到bridge.！',
-			welcomeDescription:
-				'请创建你的第一个项目以开始使用。',
+			welcomeDescription: '请创建你的第一个项目以开始使用。',
 			omitPack: '忽略',
 			selectedPack: '已选中',
 			title: '创建项目',
@@ -426,12 +411,9 @@ export default {
 			projectPrefix: '项目前缀',
 			projectAuthor: '项目作者',
 			projectTargetVersion: '项目目标版本',
-			rpAsBpDependency:
-				'将资源包注册为行为包依赖',
-			bpAsRpDependency:
-				'将行为包注册为资源包依赖',
-			useLangForManifest:
-				'将包的名称/描述直接添加到清单',
+			rpAsBpDependency: '将资源包注册为行为包依赖',
+			bpAsRpDependency: '将行为包注册为资源包依赖',
+			useLangForManifest: '将包的名称/描述直接添加到清单',
 			create: 'Create',
 			saveCurrentProject:
 				'你想在创建新项目之前保存当前项目吗？任何未保存的更改都将丢失！',
@@ -444,8 +426,7 @@ export default {
 					},
 					tick: {
 						name: 'tick.json',
-						description:
-							'定义哪些函数是每刻执行的',
+						description: '定义哪些函数是每刻执行的',
 					},
 					skins: {
 						name: 'skins.json',
@@ -453,8 +434,7 @@ export default {
 					},
 					blocks: {
 						name: 'blocks.json',
-						description:
-							'用于定义方块的多个面如何组合为同一个方块',
+						description: '用于定义方块的多个面如何组合为同一个方块',
 					},
 					terrainTexture: {
 						name: 'terrain_texture.json',
@@ -470,13 +450,11 @@ export default {
 					},
 					biomesClient: {
 						name: 'biomes_client.json',
-						description:
-							'用于定义生物群系特定效果的渲染方式',
+						description: '用于定义生物群系特定效果的渲染方式',
 					},
 					sounds: {
 						name: 'sounds.json',
-						description:
-							'用于定义特定游戏功能的声音',
+						description: '用于定义特定游戏功能的声音',
 					},
 					soundDefinitions: {
 						name: 'sound_definitions.json',
@@ -489,15 +467,13 @@ export default {
 		createPreset: {
 			title: '创建预设',
 			searchPresets: '搜索预设...',
-			overwriteFiles:
-				'此预设会覆盖一个或多个现有文件。你要继续吗？',
+			overwriteFiles: '此预设会覆盖一个或多个现有文件。你要继续吗？',
 			overwriteFilesConfirm: '继续',
 			overwriteUnsavedChanges:
 				'此预设会用未保存的更改覆盖一个或多个文件。你要继续吗？',
 			overwriteUnsavedChangesConfirm: '继续',
 			validationRule: {
-				alphanumeric:
-					'你只能使用字母、数字或下划线',
+				alphanumeric: '你只能使用字母、数字或下划线',
 				lowercase: '你只能使用小写字母',
 				required: '该字段是必需的',
 				noEmptyFolderNames: '文件夹名称不能为空',
@@ -531,10 +507,8 @@ export default {
 			},
 			openNewProject: {
 				name: '打开项目',
-				description:
-					'通过选择相应的.brproject文件以打开另一个项目。',
-				saveCurrentProject:
-					'你想在加载新项目之前保存当前项目吗？',
+				description: '通过选择相应的.brproject文件以打开另一个项目。',
+				saveCurrentProject: '你想在加载新项目之前保存当前项目吗？',
 			},
 			wrongFileType: '项目必须是一个.brproject或.mcaddon文件',
 			addPack: '添加包',
@@ -557,15 +531,13 @@ export default {
 				},
 				quantityFound: {
 					name: '数量',
-					description:
-						'运行战利品表，直到找到具有指定的物品堆叠数量',
+					description: '运行战利品表，直到找到具有指定的物品堆叠数量',
 				},
 			},
 			killConditions: {
 				looting: {
 					name: '抢夺附魔',
-					description:
-						'用于运行表的抢夺级别，0表示没有抢夺',
+					description: '用于运行表的抢夺级别，0表示没有抢夺',
 				},
 			},
 		},
@@ -580,7 +552,7 @@ export default {
 			restartDevServer: {
 				name: '重启开发服务器',
 				description:
-					"您确定要重新启动编译器的开发服务器吗？这可能需要一些时间，具体取决于项目的大小。重新启动编译器会从com.mojang文件夹中删除你的附加包，并基于你的bridge.文件夹将其重新编译！",
+					'您确定要重新启动编译器的开发服务器吗？这可能需要一些时间，具体取决于项目的大小。重新启动编译器会从com.mojang文件夹中删除你的附加包，并基于你的bridge.文件夹将其重新编译！',
 			},
 			createPreset: '新建文件',
 			projectConfig: {
@@ -614,8 +586,7 @@ export default {
 				delete: {
 					name: '删除',
 					description: '删除一个文件或文件夹',
-					confirmText:
-						"你确定要删除此文件吗？你之后将无法使其恢复！",
+					confirmText: '你确定要删除此文件吗？你之后将无法使其恢复！',
 				},
 				rename: {
 					name: '重命名',
@@ -627,10 +598,8 @@ export default {
 				},
 				viewCompilerOutput: {
 					name: '查看编译器输出',
-					description:
-						'查看该文件当前的编译器输出',
-					fileMissing:
-						"看起来该文件还未被编译。",
+					description: '查看该文件当前的编译器输出',
+					fileMissing: '看起来该文件还未被编译。',
 				},
 				revealFilePath: {
 					name: '显示文件路径',
@@ -657,25 +626,22 @@ export default {
 				name: '侧边栏',
 				sidebarRight: {
 					name: '侧边栏右置',
-					description:
-						'将侧边栏移动到屏幕右侧',
+					description: '将侧边栏移动到屏幕右侧',
 				},
 				sidebarSize: {
 					name: '侧边栏大小',
-					description:
-						'更改展开的侧边栏区域的宽度。',
+					description: '更改展开的侧边栏区域的宽度。',
 				},
 				shrinkSidebarElements: {
 					name: '收缩侧边栏元素',
-					description:
-						"收缩bridge.的侧边栏元素的大小",
+					description: '收缩bridge.的侧边栏元素的大小',
 				},
 			},
 			appearance: {
 				name: '外观',
 				colorScheme: {
 					name: '配色方案',
-					description: "选择bridge.的UI的配色方案",
+					description: '选择bridge.的UI的配色方案',
 				},
 				darkTheme: {
 					name: '深色主题',
@@ -687,31 +653,27 @@ export default {
 				},
 				localDarkTheme: {
 					name: '局部深色主题',
-					description:
-						'为当前活动的项目选择一个深色主题',
+					description: '为当前活动的项目选择一个深色主题',
 				},
 				localLightTheme: {
 					name: '局部浅色主题',
-					description:
-						'为当前活动的项目选择一个浅色主题',
+					description: '为当前活动的项目选择一个浅色主题',
 				},
 				fontSize: {
 					name: '字体大小',
-					description: "更改bridge.的文本的字体大小",
+					description: '更改bridge.的文本的字体大小',
 				},
 				editorFontSize: {
 					name: '代码字体大小',
-					description:
-						"更改bridge.的代码编辑器的字体大小",
+					description: '更改bridge.的代码编辑器的字体大小',
 				},
 				editorFont: {
 					name: '代码字体',
-					description: "更改bridge.的代码编辑器的字体",
+					description: '更改bridge.的代码编辑器的字体',
 				},
 				font: {
 					name: '字体',
-					description:
-						"更改在bridge.的用户界面中使用的字体",
+					description: '更改在bridge.的用户界面中使用的字体',
 				},
 			},
 			general: {
@@ -736,8 +698,7 @@ export default {
 				},
 				openLinksInBrowser: {
 					name: '在默认浏览器中打开链接',
-					description:
-						'在默认浏览器而非本地应用程序窗口中打开链接',
+					description: '在默认浏览器而非本地应用程序窗口中打开链接',
 				},
 				restoreTabs: {
 					name: '回复标签页',
@@ -750,16 +711,14 @@ export default {
 				},
 				openProjectChooserOnAppStartup: {
 					name: '打开项目选择器',
-					description:
-						'启动bridge.时自动打开项目选择器',
+					description: '启动bridge.时自动打开项目选择器',
 				},
 			},
 			developer: {
 				name: '开发者',
 				simulateOS: {
 					name: '模拟操作系统',
-					description:
-						'模拟不同的操作系统以测试平台特定的行为',
+					description: '模拟不同的操作系统以测试平台特定的行为',
 				},
 				devMode: {
 					name: '开发者模式',
@@ -791,7 +750,7 @@ export default {
 				bridgePredictions: {
 					name: 'bridge.预测',
 					description:
-						"启用bridge.预测可以让应用程序智能地决定是否在bridge.的树状编辑器中添加值或对象。这可以显著简化JSON编辑",
+						'启用bridge.预测可以让应用程序智能地决定是否在bridge.的树状编辑器中添加值或对象。这可以显著简化JSON编辑',
 				},
 				bracketPairColorization: {
 					name: '括号对着色',
@@ -803,30 +762,26 @@ export default {
 				},
 				wordWrapColumns: {
 					name: '自动换行列数',
-					description:
-						'定义编辑器应该在多少列之后换行',
+					description: '定义编辑器应该在多少列之后换行',
 				},
 				compactTabDesign: {
 					name: '紧凑标签页设计',
-					description:
-						'以更紧凑的方式显示标签页系统内部的标签页',
+					description: '以更紧凑的方式显示标签页系统内部的标签页',
 				},
 				automaticallyOpenTreeNodes: {
 					name: '自动打开树状节点',
 					description:
-						"在bridge.的树状编辑器中，选择节点时会自动打开它们",
+						'在bridge.的树状编辑器中，选择节点时会自动打开它们',
 				},
 				dragAndDropTreeNodes: {
 					name: '拖放树状节点',
-					description:
-						"禁用/启用bridge.的树状编辑器中树状节点的拖放",
+					description: '禁用/启用bridge.的树状编辑器中树状节点的拖放',
 				},
 			},
 		},
 		projectFolder: {
 			title: '项目文件夹',
-			content:
-				'bridge.需要访问其项目文件夹才能正常工作。',
+			content: 'bridge.需要访问其项目文件夹才能正常工作。',
 		},
 		extensionStore: {
 			title: '扩展商店',
@@ -834,13 +789,12 @@ export default {
 			deleteExtension: '删除扩展',
 			activateExtension: '激活扩展',
 			deactivateExtension: '停用扩展',
-			offlineError:
-				'无法加载扩展。请确认你的设备具有可用的网络连接。',
+			offlineError: '无法加载扩展。请确认你的设备具有可用的网络连接。',
 			compilerPluginDownload: {
 				compilerPlugins: '编译器插件',
 				title: '下载编译器插件',
 				description:
-					"你刚刚下载了一个新的编译器插件。请确保将其添加到你的编译器配置文件中，否则它不会有任何效果。",
+					'你刚刚下载了一个新的编译器插件。请确保将其添加到你的编译器配置文件中，否则它不会有任何效果。',
 				openConfig: '打开配置',
 			},
 		},
@@ -848,8 +802,7 @@ export default {
 			title: '选择安装位置',
 		},
 		unsavedFile: {
-			description:
-				'你想在关闭之前保存对该文件的更改吗？',
+			description: '你想在关闭之前保存对该文件的更改吗？',
 			save: '保存并关闭',
 		},
 		browserUnsupported: {
@@ -887,8 +840,7 @@ export default {
 			},
 			loadingSchemas: {
 				name: '加载自动补全',
-				description:
-					'bridge.当前正在加载其自动补全数据。',
+				description: 'bridge.当前正在加载其自动补全数据。',
 			},
 		},
 	},
@@ -909,8 +861,7 @@ export default {
 		},
 	},
 	comMojang: {
-		folderDropped:
-			'你想将此文件夹设置为默认的com.mojang文件夹吗？',
+		folderDropped: '你想将此文件夹设置为默认的com.mojang文件夹吗？',
 		title: '访问文件夹“com.mojang”',
 		permissionRequest:
 			'bridge.需要访问你的“com.mojang”文件夹才能将项目编译到其中。',
@@ -931,8 +882,7 @@ export default {
 		includeFiles: '要包含的文件',
 		excludeFiles: '要排除的文件',
 		noResults: '未找到结果。',
-		noSearch:
-			'开始输入后，搜索查询的结果将显示在此处。',
+		noSearch: '开始输入后，搜索查询的结果将显示在此处。',
 	},
 	preview: {
 		name: '预览',
@@ -949,8 +899,7 @@ export default {
 		noGeometry:
 			'在此文件中找不到有效的几何。确保您的JSON有效、文件结构正确且具有所提供标识符的几何存在。',
 		lootTableSimulator: {
-			emptyLootOutput:
-				'输出为空，尝试运行战利品表以收集结果。',
+			emptyLootOutput: '输出为空，尝试运行战利品表以收集结果。',
 			data: {
 				value: '数据值',
 				enchantments: '魔咒',
@@ -1006,7 +955,7 @@ export default {
 			editorType: {
 				name: '选择编辑器类型',
 				description:
-					"你想如何编辑JSON文件？你可以稍后在bridge.的设置中更改你的选择！",
+					'你想如何编辑JSON文件？你可以稍后在bridge.的设置中更改你的选择！',
 				rawText: {
 					name: '原始文本',
 					description:
@@ -1036,37 +985,37 @@ export default {
 		errors: {
 			emptyComplexConstructor: '意外的空的复杂选择器！',
 			invalidSelectorAttribute: {
-				part1: "无效的选择器特性‘",
-				part2: "’！",
+				part1: '无效的选择器特性‘',
+				part2: '’！',
 			},
 			expectedEqualsButNothing: '预期的等号但所得为空！',
 			expectedEquals: '预期的等号！',
 			expectedValueButNothing: '预期的值但所得为空！',
 			attributeNegationSupport: {
-				part1: "特性‘",
-				part2: "’不支持否定！",
+				part1: '特性‘',
+				part2: '’不支持否定！',
 			},
 			multipleInstancesNever: {
-				part1: "不允许出现特性‘",
-				part2: "’的多个实例！",
+				part1: '不允许出现特性‘',
+				part2: '’的多个实例！',
 			},
 			multipleInstancesNegated: {
-				part1: "在否定时不允许出现特性‘",
-				part2: "’的多个实例！",
+				part1: '在否定时不允许出现特性‘',
+				part2: '’的多个实例！',
 			},
 			selectorAttributeTypeMismatch: {
-				part1: "预期的值类型‘",
-				part2: "’，但所得为‘",
-				part3: "’！",
+				part1: '预期的值类型‘',
+				part2: '’，但所得为‘',
+				part3: '’！',
 			},
 			selectorValueNotValid: {
-				part1: "值‘",
-				part2: "’不是预期值之一！",
+				part1: '值‘',
+				part2: '’不是预期值之一！',
 			},
 			expectedComma: '选择器特性之间需要逗号！',
 			unclosedString: '未闭合的字符串！',
 			invalidCommand: {
-				part1: "‘",
+				part1: '‘',
 				part2: '’不是一个有效的命令！',
 			},
 			expectedLetterAfterAtButNothing: '在@之后出现了预期的的字母！',
@@ -1074,7 +1023,6 @@ export default {
 			invalidSelector: {
 				part1: '‘@',
 				part2: '’不是一个有效的选择器！',
-',
 			},
 			unexpectedOpenSquareBracket: '意外的‘[’！',
 			selectorNotBeforeOpenSquareBracketButNothing:
@@ -1084,36 +1032,30 @@ export default {
 			unexpectedCloseSquareBracket: '意外的‘]’！',
 			noValidCommandVarsFound: {
 				part1: '找不到有效的命令变体！参数‘',
-				part2: "’可能无效！它的类型是‘",
-				part3:
-					"’，但当前的变体树不支持该类型。",
+				part2: '’可能无效！它的类型是‘',
+				part3: '’，但当前的变体树不支持该类型。',
 			},
 			noValidCommandVarsFoundEnd: {
-				part1:
-					'找不到有效的命令变体！您可能遗漏了一些参数或参数‘',
+				part1: '找不到有效的命令变体！您可能遗漏了一些参数或参数‘',
 				part2: '’可能无效！',
 			},
 		},
 		warnings: {
 			schemaFamily: {
-				part1: "找不到父族‘",
-				part2:
-					"’。这可能是一个错误，或者该父族来自另一个附加包。",
+				part1: '找不到父族‘',
+				part2: '’。这可能是一个错误，或者该父族来自另一个附加包。',
 			},
 			schemaType: {
-				part1: "找不到类型‘",
-				part2:
-					"’。这可能是一个错误，或者该类型来自另一个附加包。",
+				part1: '找不到类型‘',
+				part2: '’。这可能是一个错误，或者该类型来自另一个附加包。',
 			},
 			schemaTag: {
-				part1: "找不到标签‘",
-				part2:
-					"’。这可能是一个错误，或者该标签来自另一个附加包。",
+				part1: '找不到标签‘',
+				part2: '’。这可能是一个错误，或者该标签来自另一个附加包。',
 			},
 			schemaValue: {
-				part1: "找不到模式值‘",
-				part2:
-					"’。这可能是一个错误，或者该模式值来自另一个附加包。",
+				part1: '找不到模式值‘',
+				part2: '’。这可能是一个错误，或者该模式值来自另一个附加包。',
 			},
 		},
 	},
