cefda2c3cb06ec172f4bbc221108223784dfb4b2
solvedDev <solvedDev@gmail.com>
2022-03-07 19:16:07 +0100

    fix: fileSave.any event not firing correctly

--- src/components/Projects/Project/FileChangeRegistry.ts
+++ src/components/Projects/Project/FileChangeRegistry.ts
@@ -5,14 +5,13 @@ export class FileChangeRegistry extends EventSystem<File> {
 		super([], true)
 	}
 	dispatch(name: string, fileContent: File) {
-		if (!this.hasEvent(name)) {
-			// We always want to dispatch the event for "any" file changed
-			this.any.dispatch([name, fileContent])
-			return
-		}
+		// We always want to dispatch the event for "any" file changed
+		this.any.dispatch([name, fileContent])
 
-		// Specific events only get triggered when a listener is registered already
-		super.dispatch(name, fileContent)
+		if (this.hasEvent(name)) {
+			// Specific events only get triggered when a listener is registered already
+			super.dispatch(name, fileContent)
+		}
 	}
 	on(name: string, listener: (data: File) => void) {
 		if (!this.hasEvent(name)) this.create(name)
