f51f31c238029285af6ff818f8aaa5d77cb6a1f9
solvedDev <solvedDev@gmail.com>
2022-05-28 15:26:26 +0200

    fix: remove console.log

--- src/components/Editors/BlockModel/Tab.ts
+++ src/components/Editors/BlockModel/Tab.ts
@@ -139,7 +139,6 @@ export class BlockModelTab extends GeometryPreviewTab {
 
 	async createModel() {
 		await super.createModel()
-		console.log('CREATE MODEL', this.previewOptions.loadComponents)
 
 		if (this.previewOptions.loadComponents)
 			this.createOutlineBoxes([
@@ -160,9 +159,6 @@ export class BlockModelTab extends GeometryPreviewTab {
 		return components
 	}
 	loadCollisionBoxes(type: 'pick' | 'entity') {
-		console.log(
-			this.findComponents(this.blockJson, `minecraft:${type}_collision`)
-		)
 		return this.findComponents(
 			this.blockJson,
 			`minecraft:${type}_collision`
