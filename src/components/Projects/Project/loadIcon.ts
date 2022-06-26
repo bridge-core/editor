5b7448d85d011580f5005e053762ab859cd93adb
solvedDev <solvedDev@gmail.com>
2022-06-26 20:27:46 +0200

    fix: project icon loading

--- src/components/Projects/Project/loadIcon.ts
+++ src/components/Projects/Project/loadIcon.ts
@@ -2,19 +2,17 @@ import { TPackTypeId } from 'mc-project-core'
 import type { Project } from './Project'
 import { FileSystem } from '/@/components/FileSystem/FileSystem'
 import { loadAsDataURL } from '/@/utils/loadAsDataUrl'
+import { join } from '/@/utils/path'
 
 export async function loadIcon(project: Project, fileSystem: FileSystem) {
 	const config = project.config
 
-	const definedPacks = Object.values(config.getAvailablePacks())
+	const packPaths = Object.values(config.getAvailablePacks())
 
-	if (definedPacks.length === 0) return
+	if (packPaths.length === 0) return
 
 	return await loadAsDataURL(
-		project.config.resolvePackPath(
-			<TPackTypeId>definedPacks[0],
-			'pack_icon.png'
-		),
+		join(packPaths[0], 'pack_icon.png'),
 		fileSystem
 	).catch(() => undefined)
 }
