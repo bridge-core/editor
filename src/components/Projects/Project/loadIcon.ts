e7bee96a1ec605a9c5ecc0020f44002cc8cd600a
solvedDev <solvedDev@gmail.com>
2022-06-26 20:03:58 +0200

    upd: optimize loadIcon()

--- src/components/Projects/Project/loadIcon.ts
+++ src/components/Projects/Project/loadIcon.ts
@@ -1,30 +1,20 @@
+import { TPackTypeId } from 'mc-project-core'
 import type { Project } from './Project'
 import { FileSystem } from '/@/components/FileSystem/FileSystem'
 import { loadAsDataURL } from '/@/utils/loadAsDataUrl'
 
 export async function loadIcon(project: Project, fileSystem: FileSystem) {
-	try {
-		return await loadAsDataURL(
-			project.config.resolvePackPath('behaviorPack', 'pack_icon.png'),
-			fileSystem
-		)
-	} catch {}
-	try {
-		return await loadAsDataURL(
-			project.config.resolvePackPath('resourcePack', 'pack_icon.png'),
-			fileSystem
-		)
-	} catch {}
-	try {
-		return await loadAsDataURL(
-			project.config.resolvePackPath('skinPack', 'pack_icon.png'),
-			fileSystem
-		)
-	} catch {}
-	try {
-		return await loadAsDataURL(
-			project.config.resolvePackPath('worldTemplate', 'pack_icon.png'),
-			fileSystem
-		)
-	} catch {}
+	const config = project.config
+
+	const definedPacks = Object.values(config.getAvailablePacks())
+
+	if (definedPacks.length === 0) return
+
+	return await loadAsDataURL(
+		project.config.resolvePackPath(
+			<TPackTypeId>definedPacks[0],
+			'pack_icon.png'
+		),
+		fileSystem
+	).catch(() => undefined)
 }
