cf30f68d20f23e746f079a4e4042447af8d9043f
solvedDev <solvedDev@gmail.com>
2022-06-26 20:03:36 +0200

    upd: optimize loadPacks()

--- src/components/Projects/Project/loadPacks.ts
+++ src/components/Projects/Project/loadPacks.ts
@@ -2,7 +2,6 @@ import { App } from '/@/App'
 import { IPackType, TPackTypeId } from '/@/components/Data/PackType'
 import { loadManifest } from './loadManifest'
 import type { Project } from './Project'
-import { defaultPackPaths } from './Config'
 
 export interface IPackData extends IPackType {
 	version: number[]
@@ -10,36 +9,23 @@ export interface IPackData extends IPackType {
 }
 
 export async function loadPacks(app: App, project: Project) {
-	await App.packType.ready.fired
-
-	const availablePackIds = <TPackTypeId[]>(
-		Object.keys(project.config.get().packs ?? defaultPackPaths)
-	)
-
 	const packs: IPackData[] = []
+	const config = project.config
+	const definedPacks = config.getAvailablePacks()
 
-	await Promise.allSettled(
-		availablePackIds.map(async (packId) => {
-			const packPath = project.config.resolvePackPath(packId)
-			if (!(await app.fileSystem.directoryExists(packPath))) return
+	for (const [packId, packPath] of Object.entries(definedPacks)) {
+		// Load pack manifest
+		let manifest: any = {}
+		try {
+			manifest = await loadManifest(app, packPath)
+		} catch {}
 
-			// Check whether handle is a valid pack
-			const packType = App.packType.getFromId(packId)
-			if (!packType) return
-
-			// Load pack manifest
-			let manifest: any = {}
-			try {
-				manifest = await loadManifest(app, packPath)
-			} catch {}
-
-			packs.push({
-				...packType,
-				packPath,
-				version: manifest?.header?.version ?? [1, 0, 0],
-			})
+		packs.push({
+			...App.packType.getFromId(<TPackTypeId>packId)!,
+			packPath,
+			version: manifest?.header?.version ?? [1, 0, 0],
 		})
-	)
+	}
 
 	return packs
 }
