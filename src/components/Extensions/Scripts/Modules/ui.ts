2f3f4f45fffd0972cd3166c0991b06fcce11a72f
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-06-26 15:49:27 +0100

    upd: expose BridgeSheet component to extensions

--- src/components/Extensions/Scripts/Modules/ui.ts
+++ src/components/Extensions/Scripts/Modules/ui.ts
@@ -1,11 +1,13 @@
 import { IModuleConfig } from '../types'
 import BaseWindow from '/@/components/Windows/Layout/BaseWindow.vue'
 import SidebarWindow from '/@/components/Windows/Layout/SidebarWindow.vue'
+import BridgeSheet from '/@/components/UIElements/Sheet.vue'
 
 export const UIModule = ({ uiStore }: IModuleConfig) => ({
 	...uiStore?.UI,
 	BuiltIn: {
 		BaseWindow,
 		SidebarWindow,
+		BridgeSheet,
 	},
 })
