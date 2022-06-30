a145b71105efac0ef6bc832c18866b369dc07627
solvedDev <solveddev@gmail.com>
2022-06-30 18:27:06 +0200

    upd(@bridge/command-bar): expect action config instead of action

--- src/components/Extensions/Scripts/Modules/CommandBar.ts
+++ src/components/Extensions/Scripts/Modules/CommandBar.ts
@@ -1,11 +1,12 @@
 import { IModuleConfig } from '../types'
-import { SimpleAction } from '/@/components/Actions/SimpleAction'
+import { SimpleAction, IActionConfig } from '/@/components/Actions/SimpleAction'
 
 export const CommandBarExtensionItems = new Set<SimpleAction>()
 
 export const CommandBarModule = ({ disposables }: IModuleConfig) => {
 	return {
-		registerAction(action: SimpleAction) {
+		registerAction(actionConfig: IActionConfig) {
+			const action = new SimpleAction(actionConfig)
 			CommandBarExtensionItems.add(action)
 
 			const disposable = {
