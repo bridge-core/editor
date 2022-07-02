2bffac5e5b04f60c1280fa79d14219775b392db8
solvedDev <solveddev@gmail.com>
2022-07-02 22:45:17 +0200

    upd: use new ext -> icon map for tab icons

--- src/components/TabSystem/CommonTab.ts
+++ src/components/TabSystem/CommonTab.ts
@@ -7,6 +7,7 @@ import { SimpleAction } from '/@/components/Actions/SimpleAction'
 import { EventDispatcher } from '../Common/Event/EventDispatcher'
 import { AnyFileHandle } from '../FileSystem/Types'
 import { shareFile } from '../StartParams/Action/openRawFile'
+import { getDefaultFileIcon } from '/@/utils/file/getIcon'
 
 export abstract class Tab<TRestoreData = any> extends Signal<Tab> {
 	abstract component: Vue.Component
@@ -90,7 +91,10 @@ export abstract class Tab<TRestoreData = any> extends Signal<Tab> {
 		return this.path.split('/').slice(2).join('/')
 	}
 	get icon() {
-		return App.fileType.get(this.getPath())?.icon ?? 'mdi-file-outline'
+		return (
+			App.fileType.get(this.getPath())?.icon ??
+			getDefaultFileIcon(this.getPath())
+		)
 	}
 	get iconColor() {
 		return App.packType.get(this.getPath())?.color
