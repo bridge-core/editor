996a0be20bb8a8de6f137bd6c92c92c4eb8f9bd7
solvedDev <solveddev@gmail.com>
2022-07-05 22:02:13 +0200

    upd(DenoConfig): use raw github link because deno registry is broken atm

--- src/components/Projects/CreateProject/Files/DenoConfig.ts
+++ src/components/Projects/CreateProject/Files/DenoConfig.ts
@@ -11,7 +11,7 @@ export class CreateDenoConfig extends CreateFile {
 			{
 				tasks: {
 					setup:
-						'deno install -A --reload -f -n dash_compiler https://deno.land/x/dash_compiler/mod.ts',
+						'deno install -A --reload -f -n dash_compiler https://raw.githubusercontent.com/bridge-core/deno-dash-compiler/main/mod.ts',
 					watch:
 						'dash_compiler build --mode development && dash_compiler watch',
 					build: 'dash_compiler build',
