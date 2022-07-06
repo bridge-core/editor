f2f3212337c6a0210bec64833215652af91d0491
solvedDev <solveddev@gmail.com>
2022-07-06 12:13:43 +0200

    upd: WIP open with snowstorm

--- src/components/UIElements/DirectoryViewer/ContextMenu/Actions/OpenWith/Snowstorm.ts
+++ src/components/UIElements/DirectoryViewer/ContextMenu/Actions/OpenWith/Snowstorm.ts
@@ -1,5 +1,8 @@
 import { FileWrapper } from '/@/components/UIElements/DirectoryViewer/FileView/FileWrapper'
 import { App } from '/@/App'
+import json5 from 'json5'
+import { strFromU8, zlibSync } from 'fflate'
+import { IframeTab } from '/@/components/Editors/IframeTab/IframeTab'
 
 export const SnowstormAction = (fileWrapper: FileWrapper) => {
 	if (!fileWrapper.name.endsWith('.json')) return null
@@ -19,6 +22,25 @@ export const SnowstormAction = (fileWrapper: FileWrapper) => {
 				const app = await App.getApp()
 				const tabSystem = app.tabSystem
 				if (!tabSystem) return
+
+				const file = await fileWrapper.handle.getFile()
+				const particleJson = json5.parse(await file.text())
+				const rawParticle = new Uint8Array(await file.arrayBuffer())
+
+				const base64 = btoa(
+					strFromU8(zlibSync(rawParticle, { level: 9 }), true)
+				)
+				const url = new URL('https://snowstorm.app/')
+				url.searchParams.set('loadParticle', base64)
+
+				tabSystem.add(
+					new IframeTab(tabSystem, {
+						icon: 'mdi-snowflake',
+						name: `Snowstorm: ${fileWrapper.name}`,
+						url: url.href,
+						iconColor: 'primary',
+					})
+				)
 			},
 		}
 
