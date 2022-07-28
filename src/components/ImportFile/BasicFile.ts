b4f48bf41b896cd5f52b5185ba1a7ec36f119e6f
solvedDev <solvedDev@gmail.com>
2022-07-28 22:02:06 +0200

    fix: file type guessing Also fixes an issue where no valid file type guess would result in "../../../" being set as a path.
    closes #542

--- src/components/ImportFile/BasicFile.ts
+++ src/components/ImportFile/BasicFile.ts
@@ -72,12 +72,15 @@ export class BasicFileImporter extends FileImporter {
 
 	protected async onSave(fileHandle: AnyFileHandle) {
 		const app = await App.getApp()
+
+		const guessedFolder = await App.fileType.guessFolder(fileHandle)
+
 		// Allow user to change file path that the file is saved to
 		const filePathWindow = new FilePathWindow({
 			fileName: fileHandle.name,
-			startPath: app.project.relativePath(
-				(await App.fileType.guessFolder(fileHandle)) ?? ''
-			),
+			startPath: guessedFolder
+				? app.project.relativePath(guessedFolder)
+				: '',
 			isPersistent: false,
 		})
 		filePathWindow.open()
