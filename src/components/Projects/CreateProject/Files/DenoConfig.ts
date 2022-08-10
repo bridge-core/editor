5ac3aa5499b7949c2862eb3ceedcaa85ba5b83df
solvedDev <solvedDev@gmail.com>
2022-08-10 12:18:57 +0200

    upd: "deno task setup" -> "deno task install_dash"

--- src/components/Projects/CreateProject/Files/DenoConfig.ts
+++ src/components/Projects/CreateProject/Files/DenoConfig.ts
@@ -10,10 +10,9 @@ export class CreateDenoConfig extends CreateFile {
 			`deno.json`,
 			{
 				tasks: {
-					setup:
+					install_dash:
 						'deno install -A --reload -f -n dash_compiler https://raw.githubusercontent.com/bridge-core/deno-dash-compiler/main/mod.ts',
-					watch:
-						'dash_compiler build --mode development && dash_compiler watch',
+					watch: 'dash_compiler build --mode development && dash_compiler watch',
 					build: 'dash_compiler build',
 				},
 			},
