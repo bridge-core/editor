b9dc2dcecb4484ea94ea615986357313b55e75e0
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-09-01 12:07:38 +0100

    upd: parse rgb color formats

--- src/components/Languages/Json/ColorPicker/format.ts
+++ src/components/Languages/Json/ColorPicker/format.ts
@@ -1,5 +1,27 @@
 import { Color } from './Color'
 
+export const colorFormats = [
+	'hexa',
+	'ahex',
+	'hex',
+	'rgbDec',
+	'rgbaDec',
+	'rgb',
+	'rgba',
+]
+
+export function parseColor(format: string, color: string) {
+	switch (format) {
+		case 'ahex':
+		case 'hexa':
+		case 'hex':
+			return parseHex(color)
+		case 'rgb':
+		case 'rgba':
+			return parseRgb(color)
+	}
+}
+
 export function parseHex(hex: string) {
 	// Ignore strings without # as they will not be a hex color value
 	if (!hex.startsWith('#')) return
@@ -71,4 +93,33 @@ export function parseHex(hex: string) {
 export function toTwoDigitHex(value: number) {
 	const hex = value.toString(16)
 	return hex.length !== 2 ? '0' + hex : hex
+}
+
+export function parseRgb(color: string) {
+	let raw
+	try {
+		raw = JSON.parse(color)
+	} catch {
+		return
+	}
+
+	// Ignore if not an array or is not a valid length
+	if (!Array.isArray(raw) || (raw.length !== 3 && raw.length !== 4)) return
+
+	// Decimal array, 0-1
+	if (raw[0] <= 1 && raw[1] <= 1 && raw[2] <= 1) {
+		return new Color({
+			red: raw[0],
+			green: raw[1],
+			blue: raw[2],
+			alpha: raw[3] ?? 1,
+		})
+	}
+
+	return new Color({
+		red: raw[0] / 255,
+		green: raw[1] / 255,
+		blue: raw[2] / 255,
+		alpha: raw[3] ? raw[3] / 255 : 1,
+	})
 }
