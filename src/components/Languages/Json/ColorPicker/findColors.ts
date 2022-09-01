1a1160cb975719c0e23eb27f6fcac76406b845a1
Joelant05 <64587014+Joelant05@users.noreply.github.com>
2022-09-01 12:06:02 +0100

    upd: new system for finding colors

---
+++ src/components/Languages/Json/ColorPicker/findColors.ts
@@ -0,0 +1,117 @@
+import { isMatch } from 'bridge-common-utils'
+import type { JSONPath } from 'jsonc-parser'
+import type { editor, languages } from 'monaco-editor'
+import { parseColor } from './format'
+import { App } from '/@/App'
+import { useJsoncParser } from '/@/utils/libs/useJsoncParser'
+import { useMonaco } from '/@/utils/libs/useMonaco'
+import { getJsonWordAtPosition } from '/@/utils/monaco/getJsonWord'
+import { getArrayValueAtPosition } from '/@/utils/monaco/getArrayValue'
+import { loadValidColors } from './loadValidColors'
+
+/**
+ * Takes a text model and detects the locations of colors in the file
+ */
+export async function findColors(model: editor.ITextModel) {
+	const { visit } = await useJsoncParser()
+	const { Range } = await useMonaco()
+
+	const content = model.getValue()
+
+	const locationPatterns = (await loadValidColors()) ?? {}
+
+	// Walk through the json file
+	const colorInfo: Promise<languages.IColorInformation>[] = []
+	visit(content, {
+		// When we reach any literal value, e.g. a string, ...
+		onLiteralValue: async (
+			value: any,
+			offset: number,
+			length: number,
+			startLine: number,
+			startCharacter: number,
+			pathSupplier: () => JSONPath
+		) => {
+			// Call the path supplier and join the JSON segments into a path
+			const path = pathSupplier().join('/')
+
+			// Iterate each color format for this file type
+			// Filter down to formats that are string only, as this is for literal values
+			for (const format of ['hex', 'hexa', 'ahex']) {
+				// Check whether the value at this JSON path matches a pattern in the valid colors file
+				if (!locationPatterns[format]) continue
+				const isValidColor = isMatch(path, locationPatterns[format])
+
+				// If this is a valid color, create a promise that will reolve when the color has been parsed and the range has been determined
+				if (isValidColor) {
+					colorInfo.push(
+						new Promise<any>(async (resolve) => {
+							const color = parseColor(format, value)
+							if (color) {
+								const { range } = await getJsonWordAtPosition(
+									model,
+									model.getPositionAt(offset + 2)
+								)
+								resolve({
+									color: color.colorInfo,
+									range: new Range(
+										range.startLineNumber,
+										range.startColumn,
+										range.endLineNumber,
+										range.endColumn + 2
+									),
+								})
+							} else resolve({})
+						})
+					)
+				}
+			}
+		},
+		onArrayBegin(
+			offset: number,
+			length: number,
+			startLine: number,
+			startCharacter: number,
+			pathSupplier: () => JSONPath
+		) {
+			// Handle similarly to when approaching a literal value
+
+			// Call the path supplier and join the JSON segments into a path
+			const path = pathSupplier().join('/')
+
+			// Iterate each color format for this file type
+			for (const format of ['rgb', 'rgba']) {
+				// Check whether the value at this JSON path matches a pattern in the valid colors file
+				if (!locationPatterns[format]) continue
+				const isValidColor = isMatch(path, locationPatterns[format])
+
+				// If this is a valid color, create a promise that will reolve when the color has been parsed and the range has been determined
+				if (isValidColor) {
+					colorInfo.push(
+						new Promise<any>(async (resolve) => {
+							const { range, word } =
+								await getArrayValueAtPosition(model, offset)
+							const color = parseColor(format, word)
+							if (color) {
+								resolve({
+									color: color.colorInfo,
+									range: new Range(
+										range.startLineNumber,
+										range.startColumn,
+										range.endLineNumber,
+										range.endColumn
+									),
+								})
+							} else resolve({})
+						})
+					)
+				}
+			}
+		},
+	})
+
+	// Await all promises for the color info and filter to ensure each color info contains the correct data
+	return (await Promise.all(colorInfo)).filter(
+		(info) => info.color && info.range
+	)
+}
