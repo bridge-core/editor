0ce193a0ddebf5011404293bb38546d9d83c80af
solvedDev <solvedDev@gmail.com>
2022-09-10 13:13:21 +0200

    feat: load window state API

--- src/components/Windows/Layout/BaseWindow.vue
+++ src/components/Windows/Layout/BaseWindow.vue
@@ -60,6 +60,9 @@
 
 			<v-card-text
 				style="overflow-y: auto"
+				:class="{
+					'd-flex align-center justify-center': isLoading,
+				}"
 				:style="{
 					height: heightUnset
 						? undefined
@@ -75,7 +78,13 @@
 						: undefined,
 				}"
 			>
-				<slot name="default" />
+				<v-progress-circular
+					v-if="isLoading"
+					indeterminate
+					color="accent"
+					size="42"
+				/>
+				<slot v-else name="default" />
 			</v-card-text>
 
 			<v-card-actions
@@ -110,6 +119,7 @@ export default {
 	},
 	props: {
 		isFullscreen: Boolean,
+		isLoading: Boolean,
 		isVisible: Object | Boolean,
 		shouldRender: Object | Boolean,
 		isPersistent: Boolean,
