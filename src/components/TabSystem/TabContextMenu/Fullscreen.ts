d536428fd06c2a0f4fca90b8c3e4dc48159b92e2
solvedDev <solvedDev@gmail.com>
2022-11-10 16:47:00 +0100

    fix: fullscreen action visibility

--- src/components/TabSystem/TabContextMenu/Fullscreen.ts
+++ src/components/TabSystem/TabContextMenu/Fullscreen.ts
@@ -14,32 +14,41 @@ export function useFullScreen() {
 	}
 }
 
-export const fullScreenAction = (addDescription = true) => ({
-	icon: 'mdi-fullscreen',
-	name: 'actions.fullscreen.name',
-	description: addDescription ? 'actions.fullscreen.description' : undefined,
-	onTrigger: () => {
-		if (!fullScreenElement) return
+export const fullScreenAction = (addDescription = true) =>
+	typeof document.body.requestFullscreen === 'function'
+		? {
+				icon: 'mdi-fullscreen',
+				name: 'actions.fullscreen.name',
+				description: addDescription
+					? 'actions.fullscreen.description'
+					: undefined,
+				onTrigger: () => {
+					if (!fullScreenElement) return
 
-		if (document.fullscreenElement) {
-			document.exitFullscreen()
-		} else {
-			fullScreenElement.requestFullscreen()
+					if (document.fullscreenElement) {
+						document.exitFullscreen()
+					} else {
+						fullScreenElement.requestFullscreen()
 
-			isInFullScreen.value = true
-			const onFullScreenChange = () => {
-				isInFullScreen.value = document.fullscreenElement != null
+						isInFullScreen.value = true
+						const onFullScreenChange = () => {
+							isInFullScreen.value =
+								document.fullscreenElement != null
 
-				// If no longer in fullscreen, remove the event listener
-				if (!isInFullScreen.value) {
-					document.removeEventListener(
-						'fullscreenchange',
-						onFullScreenChange
-					)
-				}
-			}
-			// Listen for fullscreen cancel
-			document.addEventListener('fullscreenchange', onFullScreenChange)
-		}
-	},
-})
+							// If no longer in fullscreen, remove the event listener
+							if (!isInFullScreen.value) {
+								document.removeEventListener(
+									'fullscreenchange',
+									onFullScreenChange
+								)
+							}
+						}
+						// Listen for fullscreen cancel
+						document.addEventListener(
+							'fullscreenchange',
+							onFullScreenChange
+						)
+					}
+				},
+		  }
+		: null
