6d914c02df634d1d924450c09b3b6b8760f2201e
solvedDev <solvedDev@gmail.com>
2022-11-11 18:16:01 +0100

    fix: toolbar menu

--- src/components/Toolbar/Menu/MenuList.vue
+++ src/components/Toolbar/Menu/MenuList.vue
@@ -1,10 +1,7 @@
 <template>
 	<!-- "tile" prop doesn't work on nested lists -> set border radius directly -->
 	<v-list class="app-menu" color="menu" style="border-radius: 0">
-		<template
-			v-for="(item, key, i) in elements"
-			:key="`menu.${i}.${Math.random()}`"
-		>
+		<template v-for="(item, key, i) in elements" :key="`menu.${i}`">
 			<v-divider v-if="item.type === 'divider'" />
 			<v-menu
 				v-else
@@ -18,36 +15,33 @@
 					<v-list-item
 						dense
 						v-bind="item.type === 'category' ? props : undefined"
+						color="accent"
 						@click="onClick(item)"
 						:disabled="item.isDisabled"
 					>
-						<v-list-item-icon v-if="item.icon">
+						<template v-if="item.icon" v-slot:prepend>
 							<v-icon color="accent" small>{{
 								item.icon
 							}}</v-icon>
-						</v-list-item-icon>
+						</template>
 
-						<v-list-item-content>
-							<v-list-item-title>
-								{{ t(item.name) }}
-							</v-list-item-title>
-						</v-list-item-content>
+						<v-list-item-title>
+							{{ t(item.name) }}
+						</v-list-item-title>
 
-						<v-list-item-action>
-							<v-list-item-action-text>
-								<span
-									v-if="
-										item.type !== 'category' &&
-										item.keyBinding
-									"
-								>
-									{{ item.keyBinding.toStrKeyCode() }}
-								</span>
-								<v-icon v-if="item.type === 'category'" small>
-									mdi-chevron-right
-								</v-icon>
-							</v-list-item-action-text>
-						</v-list-item-action>
+						<template v-slot:append>
+							<span
+								v-if="
+									item.type !== 'category' && item.keyBinding
+								"
+								class="ml-4 text-disabled"
+							>
+								{{ item.keyBinding.toStrKeyCode() }}
+							</span>
+							<v-icon v-if="item.type === 'category'" small>
+								mdi-chevron-right
+							</v-icon>
+						</template>
 					</v-list-item>
 				</template>
 
