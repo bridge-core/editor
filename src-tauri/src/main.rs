5f77f790f6f43ad76ed2c2f6d1e358582c6fff8b
solvedDev <solvedDev@gmail.com>
2022-11-26 00:22:59 +0100

    feat: discord rpc support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,8 @@
     windows_subsystem = "windows"
 )]
 
-use tauri::{Menu};
-
+use tauri::{Menu, Manager};
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -13,11 +13,59 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    
+    
     let menu = Menu::os_default(&"bridge. v2");
 
     tauri::Builder::default()
+        .setup(|app| {
+            // `main` here is the window label; it is defined on the window creation or under `tauri.conf.json`
+            let main_window = app.get_window("main").unwrap();
+
+            // Try to set Discord rich presence
+            match set_rich_presence() {
+                Ok(mut discord_client) => {
+                    println!("Rich presence set!");
+                    
+                    // listen to "tauri://destroyed" (emitted on the `main` window)
+                    main_window.once("tauri://destroyed", move |_| {
+                        println!("Window closes!");
+                        discord_client.close().expect("Failed to close Discord IPC");
+                    });
+                }
+                Err(e) => {
+                    println!("Error setting rich presence: {}", e);
+                }
+            };           
+      
+            Ok(())
+        })
         .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    
+}
+
+fn set_rich_presence() -> Result<DiscordIpcClient, Box<dyn std::error::Error>> {
+    let mut client = DiscordIpcClient::new("1045743881393815552")?;
+    
+    client.connect()?;
+    client.set_activity(activity::Activity::new()
+        .state("Developing add-ons...")
+        .assets(activity::Assets::new()
+            .large_image("logo_normal")
+            .large_text("bridge. v2")
+        )
+        .timestamps(activity::Timestamps::new()
+            .start(chrono::Utc::now().timestamp_millis())
+        )
+        .buttons(vec![
+            activity::Button::new("Open Editor", "https://editor.bridge-core.app/"),
+            // activity::Button::new("Website", "https://bridge-core.app/"),
+        ])
+    )?;
+
+    Ok(client)
 }
