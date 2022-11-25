c95bfac61171d4618b4a36a8a549efda8f10dd9d
solvedDev <solvedDev@gmail.com>
2022-11-25 12:33:34 +0100

    upd: improve custom update flow

--- src/components/App/TauriUpdater.ts
+++ src/components/App/TauriUpdater.ts
@@ -1,16 +1,22 @@
 import { createNotification } from '../Notifications/create'
 import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
 
 checkUpdate()
 	.then((update) => {
 		if (update.shouldUpdate) {
-			createNotification({
+			const notification = createNotification({
 				icon: 'mdi-update',
 				color: 'primary',
 				message: 'sidebar.notifications.updateAvailable.message',
 				textColor: 'white',
 				onClick: async () => {
+					// Dispose the notification
+					notification.dispose()
+					// Install the update
 					await installUpdate()
+					// ...and finally relaunch the app
+					await relaunch()
 				},
 			})
 		}
