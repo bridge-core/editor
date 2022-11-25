230dad7ee1543c8e0700eea7647bc4876702be0c
solvedDev <solvedDev@gmail.com>
2022-11-25 11:52:49 +0100

    fix: build error

--- src/components/App/TauriUpdater.ts
+++ src/components/App/TauriUpdater.ts
@@ -1,19 +1,21 @@
 import { createNotification } from '../Notifications/create'
 import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
 
-const update = await checkUpdate().catch((err: any) => {
-	console.error(`Failed to check for update: ${err}`)
-	return null
-})
-
-if (update?.shouldUpdate) {
-	createNotification({
-		icon: 'mdi-update',
-		color: 'primary',
-		message: 'sidebar.notifications.updateAvailable.message',
-		textColor: 'white',
-		onClick: async () => {
-			await installUpdate()
-		},
+checkUpdate()
+	.then((update) => {
+		if (update.shouldUpdate) {
+			createNotification({
+				icon: 'mdi-update',
+				color: 'primary',
+				message: 'sidebar.notifications.updateAvailable.message',
+				textColor: 'white',
+				onClick: async () => {
+					await installUpdate()
+				},
+			})
+		}
 	})
-}
+	.catch((err: any) => {
+		console.error(`Failed to check for update: ${err}`)
+		return null
+	})
