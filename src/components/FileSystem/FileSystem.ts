145ec0ad14e85e432970e5ba41e4e062493bb895
solvedDev <33347616+solvedDev@users.noreply.github.com>
2022-12-07 15:04:19 +0100

    feat: fast directory copying (#761)
    * feat: fast directory copying
    
    * upd: copyFolder should also use fast path

--- src/components/FileSystem/FileSystem.ts
+++ src/components/FileSystem/FileSystem.ts
@@ -8,6 +8,7 @@ import { AnyDirectoryHandle, AnyFileHandle, AnyHandle } from './Types'
 import { getStorageDirectory } from '/@/utils/getStorageDirectory'
 import { VirtualFileHandle } from './Virtual/FileHandle'
 import { VirtualDirectoryHandle } from './Virtual/DirectoryHandle'
+import { pathFromHandle } from './Virtual/pathFromHandle'
 
 export class FileSystem extends Signal<void> {
 	protected _baseDirectory!: AnyDirectoryHandle
@@ -269,19 +270,32 @@ export class FileSystem extends Signal<void> {
 		const originHandle = await this.getDirectoryHandle(originPath, {
 			create: false,
 		})
-
-		await iterateDirParallel(originHandle, async (fileHandle, filePath) => {
-			await this.copyFileHandle(
-				fileHandle,
-				await this.getFileHandle(join(destPath, filePath), true)
-			)
+		const destHandle = await this.getDirectoryHandle(destPath, {
+			create: true,
 		})
+
+		await this.copyFolderByHandle(originHandle, destHandle)
 	}
 	async copyFolderByHandle(
 		originHandle: AnyDirectoryHandle,
 		destHandle: AnyDirectoryHandle,
 		ignoreFolders?: Set<string>
 	) {
+		// Tauri build: Both handles are virtual -> Elligible for fast path
+		if (
+			import.meta.env.VITE_IS_TAURI_APP &&
+			originHandle instanceof VirtualDirectoryHandle &&
+			destHandle instanceof VirtualDirectoryHandle
+		) {
+			const src = await pathFromHandle(originHandle)
+			const dest = await pathFromHandle(destHandle)
+
+			const { invoke } = await import('@tauri-apps/api')
+			await invoke('copy_directory', { src, dest })
+
+			return
+		}
+
 		const destFs = new FileSystem(destHandle)
 
 		await iterateDirParallel(
