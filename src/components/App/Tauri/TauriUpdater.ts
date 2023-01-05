9e60de05987eed65c77bdb37f853ba17eed8ef21
solvedDev <33347616+solvedDev@users.noreply.github.com>
2023-01-05 19:56:20 +0100

    feat: update window (#834)
    * feat: update window
    
    * fix: remove console.log

--- src/components/App/Tauri/TauriUpdater.ts
+++ src/components/App/Tauri/TauriUpdater.ts
@@ -2,51 +2,67 @@ import { createNotification } from '../../Notifications/create'
 import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
 import { relaunch } from '@tauri-apps/api/process'
 import { App } from '/@/App'
+import { openUpdateWindow } from '../../Windows/Update/UpdateWindow'
+
+async function installTauriUpdate() {
+	const app = await App.getApp()
+
+	// Task to indicate background progress
+	const task = app.taskManager.create({
+		icon: 'mdi-update',
+		name: 'sidebar.notifications.installingUpdate.name',
+		description: 'sidebar.notifications.installingUpdate.description',
+
+		indeterminate: true,
+	})
+
+	// Install the update
+	await installUpdate()
+	// Dispose task
+	task.complete()
+
+	// Create a notification to indicate that the app needs to be restarted
+	createNotification({
+		icon: 'mdi-update',
+		color: 'primary',
+		message: 'sidebar.notifications.restartToApplyUpdate.message',
+		textColor: 'white',
+		onClick: async () => {
+			// ...and finally relaunch the app
+			await relaunch()
+		},
+	})
+}
 
 checkUpdate()
 	.then(async (update) => {
-		const app = await App.getApp()
+		if (!update.shouldUpdate) return
 
-		if (update.shouldUpdate) {
-			const notification = createNotification({
-				icon: 'mdi-update',
-				color: 'primary',
-				message: 'sidebar.notifications.updateAvailable.message',
-				textColor: 'white',
-				onClick: async () => {
-					// Dispose the notification
-					notification.dispose()
+		const notification = createNotification({
+			icon: 'mdi-update',
+			color: 'primary',
+			message: 'sidebar.notifications.updateAvailable.message',
+			textColor: 'white',
+			onClick: async () => {
+				// Dispose the notification
+				notification.dispose()
 
-					// Task to indicate background progress
-					const task = app.taskManager.create({
-						icon: 'mdi-update',
-						name: 'sidebar.notifications.installingUpdate.name',
-						description:
-							'sidebar.notifications.installingUpdate.description',
+				// Install the update
+				await installTauriUpdate()
+			},
+		})
 
-						indeterminate: true,
-					})
+		openUpdateWindow({
+			// Version should always be defined because we're checking update.shouldUpdate before
+			version: update.manifest?.version ?? '2.x',
+			onClick: () => {
+				// Dispose the notification
+				notification.dispose()
 
-					// Install the update
-					await installUpdate()
-					// Dispose task
-					task.complete()
-
-					// Create a notification to indicate that the app needs to be restarted
-					createNotification({
-						icon: 'mdi-update',
-						color: 'primary',
-						message:
-							'sidebar.notifications.restartToApplyUpdate.message',
-						textColor: 'white',
-						onClick: async () => {
-							// ...and finally relaunch the app
-							await relaunch()
-						},
-					})
-				},
-			})
-		}
+				// Install the update
+				installTauriUpdate()
+			},
+		})
 	})
 	.catch((err: any) => {
 		console.error(`[TauriUpdater] ${err}`)
