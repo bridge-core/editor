2667aa6ddfbfc972dd0c9a6816f7d25168fa33a1
solvedDev <solvedDev@gmail.com>
2023-01-07 14:01:01 +0100

    feat: view bridge folder

--- src/utils/getBridgeFolderPath.ts
+++ src/utils/getBridgeFolderPath.ts
@@ -1,4 +1,3 @@
-import { appLocalDataDir, join } from '@tauri-apps/api/path'
 import { get } from 'idb-keyval'
 
 let cachedPath: string | undefined = undefined
@@ -7,6 +6,8 @@ export async function getBridgeFolderPath() {
 	if (!import.meta.env.VITE_IS_TAURI_APP)
 		throw new Error(`This function is only available in Tauri apps.`)
 	if (cachedPath) return cachedPath
+
+	const { appLocalDataDir, join } = await import('@tauri-apps/api/path')
 
 	const configuredPath = await get<string | undefined>('bridgeFolderPath')
 	if (configuredPath) {
