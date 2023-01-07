21090082d3a12c313acd3d7eb7ff1e0538bda52a
solvedDev <solvedDev@gmail.com>
2023-01-07 16:39:40 +0100

    fix: type error

--- src/components/UIElements/DirectoryViewer/ContextMenu/Actions/RevealInFileExplorer.ts
+++ src/components/UIElements/DirectoryViewer/ContextMenu/Actions/RevealInFileExplorer.ts
@@ -1,11 +1,14 @@
+import { DirectoryWrapper } from '../../DirectoryView/DirectoryWrapper'
+import { FileWrapper } from '../../FileView/FileWrapper'
 import { VirtualDirectoryHandle } from '/@/components/FileSystem/Virtual/DirectoryHandle'
 import { VirtualFileHandle } from '/@/components/FileSystem/Virtual/FileHandle'
+import { BaseVirtualHandle } from '/@/components/FileSystem/Virtual/Handle'
 import { TauriFsStore } from '/@/components/FileSystem/Virtual/Stores/TauriFs'
 import { BaseWrapper } from '/@/components/UIElements/DirectoryViewer/Common/BaseWrapper'
 import { revealInFileExplorer } from '/@/utils/revealInFileExplorer'
 
 export const RevealInFileExplorer = (
-	baseWrapper: BaseWrapper<VirtualDirectoryHandle | VirtualFileHandle>
+	baseWrapper: FileWrapper | DirectoryWrapper
 ) => {
 	if (!import.meta.env.VITE_IS_TAURI_APP) return null
 
@@ -16,8 +19,11 @@ export const RevealInFileExplorer = (
 				: 'mdi-file-marker-outline',
 		name: 'actions.revealInFileExplorer.name',
 		onTrigger: async () => {
+			const handle = baseWrapper.handle
+			if (!(handle instanceof BaseVirtualHandle)) return
+
 			let path = baseWrapper.path
-			const baseStore = baseWrapper.handle.getBaseStore()
+			const baseStore = handle.getBaseStore()
 			if (!(baseStore instanceof TauriFsStore) || !path) return
 
 			revealInFileExplorer(await baseStore.resolvePath(path))
