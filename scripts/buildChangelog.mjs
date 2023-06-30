146b780393c3e9f35ed6a2af795f49c1318b3f5b
NguyenDuck <duck.duc.vit@gmail.com>
2023-06-30 08:22:46 +0700

    upd: disable build the changelog

--- scripts/buildChangelog.mjs
+++ scripts/buildChangelog.mjs
@@ -3,23 +3,20 @@ import { promises as fs } from 'fs'
 import fetch from 'node-fetch'
 
 export async function buildChangelog() {
-	let headers
-	const GITHUB_TOKEN = process.env.GITHUB_TOKEN
-
-	if (GITHUB_TOKEN !== undefined) {
-		headers = {
-			Authorization: `Bearer ${GITHUB_TOKEN}`,
-		}
-	}
-
-	const html = await fetch(
-		'https://api.github.com/repos/bridge-core/editor/releases',
-		{
-			headers: headers,
-		}
-	)
-		.then((response) => response.json())
-		.then((data) => new MarkdownIt().render(data[0].body))
-
-	await fs.writeFile('public/changelog.html', html)
+	// let headers
+	// const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+	// if (GITHUB_TOKEN !== undefined) {
+	// 	headers = {
+	// 		Authorization: `Bearer ${GITHUB_TOKEN}`,
+	// 	}
+	// }
+	// const html = await fetch(
+	// 	'https://api.github.com/repos/bridge-core/editor/releases',
+	// 	{
+	// 		headers: headers,
+	// 	}
+	// )
+	// 	.then((response) => response.json())
+	// 	.then((data) => new MarkdownIt().render(data[0].body))
+	// await fs.writeFile('public/changelog.html', html)
 }
