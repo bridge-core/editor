ef214be53914d3b43bd03995c72c8b71664ee2a7
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-11-04 00:29:29 -0500

    improve greet ui

--- src/components/Greet/Greet.vue
+++ src/components/Greet/Greet.vue
@@ -2,9 +2,9 @@
 	<main>
 		<div class="p-2 top-bar">
 			<div class="icon-buttons">
-				<v-icon size="large" color="accent">mdi-cog</v-icon>
-				<v-icon size="large" color="accent">mdi-help-circle</v-icon>
-				<v-icon size="large" color="accent">mdi-download</v-icon>
+				<v-icon size="large">mdi-cog</v-icon>
+				<v-icon size="large">mdi-help-circle</v-icon>
+				<v-icon size="large">mdi-download</v-icon>
 			</div>
 			<div
 				class="px-1 rounded-lg d-table app-version-display"
@@ -14,37 +14,28 @@
 			</div>
 		</div>
 
-		<div class="d-flex flex-column w-72">
+		<div class="d-flex flex-column projects-container">
 			<Logo
 				style="height: 160px; width: 160px"
 				class="ml-auto mr-auto mb-12"
 				alt="Logo of bridge. v2"
 			/>
-			<p class="mb-1 text-lg">Projects</p>
-			<div class="seperator" />
-			<div class="project-list">
-				<div class="project">
-					<Logo class="mr-2" style="height: 40px; width: 40px" />
-					<p>My New Bridge Project</p>
-				</div>
-				<div class="project">
-					<Logo class="mr-2" style="height: 40px; width: 40px" />
-					<p>Scripting Testing Project</p>
-				</div>
-				<div class="project">
-					<Logo class="mr-2" style="height: 40px; width: 40px" />
-					<p>Experiments</p>
+			<div class="flex justify-between">
+				<p class="mb-1 text-lg">Projects</p>
+				<div>
+					<v-icon size="large" class="mr-1">mdi-folder</v-icon>
+					<v-icon size="large">mdi-plus</v-icon>
 				</div>
 			</div>
-
-			<div class="main-buttons">
-				<div class="main-button">
-					<v-icon size="large">mdi-folder</v-icon>
-					<p>Load Projects</p>
-				</div>
-				<div class="main-button">
-					<v-icon class="smaller-icon" size="large">mdi-plus</v-icon>
-					<p>New</p>
+			<div class="seperator" />
+			<div class="project-list">
+				<div
+					class="project"
+					v-for="(project, index) in projects"
+					:key="index"
+				>
+					<Logo class="mr-3" style="height: 40px; width: 40px" />
+					<p class="text-sm">{{ project }}</p>
 				</div>
 			</div>
 		</div>
@@ -60,6 +51,15 @@ async function openChangelogWindow() {
 	const app = await App.getApp()
 	await app.windows.changelogWindow.open()
 }
+
+const projects = [
+	'My New Bridge Project',
+	'My New Bridge Project',
+	'My New Bridge Project',
+	'My New Bridge Project',
+	'My New Bridge Project',
+	'My New Bridge Project',
+]
 </script>
 
 <style scoped>
@@ -72,12 +72,23 @@ main {
 	align-items: center;
 }
 
+.projects-container {
+	width: 28rem;
+}
+
 .seperator {
 	height: 1px;
 	width: 100%;
 	background-color: var(--v-menu-base);
 
 	margin-bottom: 1rem;
+}
+
+.project-list {
+	display: flex;
+	flex-wrap: wrap;
+
+	justify-content: space-between;
 }
 
 .project {
@@ -110,36 +121,5 @@ main {
 .icon-buttons {
 	display: flex;
 	gap: 0.5rem;
-}
-
-.main-buttons {
-	display: flex;
-
-	justify-content: space-evenly;
-}
-
-.main-button {
-	display: flex;
-	align-items: center;
-	gap: 0.5rem;
-
-	background: var(--v-menu-base);
-
-	padding: 0.25rem;
-	padding-left: 0.5rem;
-	padding-right: 0.5rem;
-
-	border-radius: 4px;
-
-	margin-top: 2rem;
-}
-
-.main-button > p {
-	margin: 0;
-}
-
-.smaller-icon {
-	margin-left: -0.2rem;
-	margin-right: -0.25rem;
 }
 </style>
