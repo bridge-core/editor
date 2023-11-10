77d7e71e0b812a858e89451cc9aa958bb5ab4f41
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-11-10 14:24:08 -0600

    Fix toolbar buttons not working on mobile

--- src/components/Toolbar/Category/download.ts
+++ src/components/Toolbar/Category/download.ts
@@ -1,17 +1,22 @@
+import { vuetify } from '../../App/Vuetify'
+import { ToolbarButton } from '../ToolbarButton'
 import { App } from '/@/App'
 
 export function setupDownloadButton(app: App) {
-	App.toolbar.add({
-		type: 'button',
-		id: 'download',
-		name: 'toolbar.download.name',
-		trigger() {
-			App.openUrl(
-				'https://bridge-core.app/guide/download/',
-				undefined,
-				true
-			)
-		},
-		shouldRender: { value: !import.meta.env.VITE_IS_TAURI_APP },
-	})
+	console.log(!vuetify.framework.breakpoint.mobile)
+
+	App.toolbar.add(
+		new ToolbarButton(
+			'mdi-download',
+			'toolbar.download.name',
+			() => {
+				App.openUrl(
+					'https://bridge-core.app/guide/download/',
+					undefined,
+					true
+				)
+			},
+			{ value: !import.meta.env.VITE_IS_TAURI_APP }
+		)
+	)
 }
