e922c80cf59430498ad913487b301fcbc2706f83
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-11-18 19:52:47 -0600

    fix get data function in case there is no packs

--- src/libs/projects/Project.ts
+++ src/libs/projects/Project.ts
@@ -1,5 +1,6 @@
 import { basename, join } from '/@/libs/path'
 import { App } from '/@/App'
+import { defaultPackPaths } from 'mc-project-core'
 
 export interface ProjectData {
 	name: string
@@ -15,10 +16,38 @@ export async function validProject(path: string) {
 export async function getData(path: string): Promise<ProjectData> {
 	const fileSystem = App.instance.fileSystem
 
+	let iconDataUrl =
+		'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+
+	if (
+		await fileSystem.exists(
+			join(path, defaultPackPaths['behaviorPack'], 'pack_icon.png')
+		)
+	)
+		iconDataUrl = await fileSystem.readFileDataUrl(
+			join(path, 'BP', 'pack_icon.png')
+		)
+
+	if (
+		await fileSystem.exists(
+			join(path, defaultPackPaths['resourcePack'], 'pack_icon.png')
+		)
+	)
+		iconDataUrl = await fileSystem.readFileDataUrl(
+			join(path, 'BP', 'pack_icon.png')
+		)
+
+	if (
+		await fileSystem.exists(
+			join(path, defaultPackPaths['skinPack'], 'pack_icon.png')
+		)
+	)
+		iconDataUrl = await fileSystem.readFileDataUrl(
+			join(path, 'BP', 'pack_icon.png')
+		)
+
 	return {
 		name: basename(path),
-		icon: await fileSystem.readFileDataUrl(
-			join(path, 'BP', 'pack_icon.png')
-		),
+		icon: iconDataUrl,
 	}
 }
