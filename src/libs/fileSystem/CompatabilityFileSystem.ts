685d71745bfdc5f91ee55b6fa698fca9284ccb0a
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-12-10 02:22:31 -0600

    compiling files!

--- src/libs/fileSystem/CompatabilityFileSystem.ts
+++ src/libs/fileSystem/CompatabilityFileSystem.ts
@@ -1,6 +1,6 @@
 import { FileSystem } from 'dash-compiler'
 import { BaseFileSystem } from './BaseFileSystem'
-import { basename } from '@/libs/path'
+import { basename, join, sep } from '@/libs/path'
 
 export class CompatabilityFileSystem extends FileSystem {
 	constructor(public fileSystem: BaseFileSystem) {
@@ -8,6 +8,8 @@ export class CompatabilityFileSystem extends FileSystem {
 	}
 
 	async readFile(path: string): Promise<File> {
+		console.log('Reading file from ', path)
+
 		const content = await this.fileSystem.readFile(path)
 
 		const file = new File([new Blob([content])], basename(path))
@@ -17,9 +19,21 @@ export class CompatabilityFileSystem extends FileSystem {
 	async writeFile(path: string, content: string | Uint8Array): Promise<void> {
 		console.log('Writing file to ', path)
 
+		let currentPath = ''
+
+		const pathSlices = path.split(sep)
+		for (const piece of pathSlices.slice(0, pathSlices.length - 1)) {
+			currentPath = join(currentPath, piece)
+
+			if (!(await this.fileSystem.exists(currentPath)))
+				await this.fileSystem.makeDirectory(currentPath)
+		}
+
 		await this.fileSystem.writeFile(path, content)
 	}
 	async unlink(path: string): Promise<void> {
+		console.log('Deleting file ', path)
+
 		throw new Error('Method not implemented.')
 	}
 	async readdir(path: string): Promise<
@@ -28,6 +42,8 @@ export class CompatabilityFileSystem extends FileSystem {
 			kind: 'file' | 'directory'
 		}[]
 	> {
+		console.log('Reading directory ', path)
+
 		if (!(await this.fileSystem.exists(path)))
 			await this.fileSystem.makeDirectory(path)
 
@@ -38,6 +54,8 @@ export class CompatabilityFileSystem extends FileSystem {
 		)
 	}
 	async mkdir(path: string): Promise<void> {
+		console.log('Creating directory ', path)
+
 		throw new Error('Method not implemented.')
 	}
 	async lastModified(filePath: string): Promise<number> {
