68241866994248c8f9136fcb2ce08aa783e3c242
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-12-13 22:05:04 -0600

    begin working on preset file creation

--- src/libs/data/bedrock/PresetData.ts
+++ src/libs/data/bedrock/PresetData.ts
@@ -1,4 +1,5 @@
 import { data } from '@/App'
+import { dirname, join } from '@/libs/path'
 
 export class PresetData {
 	public presets: { [key: string]: any } = {}
@@ -15,11 +16,31 @@ export class PresetData {
 		}
 	}
 
-	public async createPreset(path: string, options: any) {
-		console.log(
-			'Creating preset',
-			path,
-			JSON.parse(JSON.stringify(options))
-		)
+	public async createPreset(presetPath: string, presetOptions: any) {
+		const preset = this.presets[presetPath]
+
+		console.log(preset)
+
+		const createFiles = preset.createFiles ?? []
+
+		for (let [templatePath, targetPath, templateOptions] of createFiles) {
+			templatePath = join(
+				dirname(presetPath.substring('file:///data/'.length)),
+				templatePath
+			)
+
+			let templateContent = await data.get(templatePath)
+
+			if (templateOptions.inject) {
+				for (const inject of templateOptions.inject) {
+					targetPath = targetPath.replace(
+						inject,
+						`{{${presetOptions[inject]}}}`
+					)
+				}
+			}
+
+			console.log(targetPath)
+		}
 	}
 }
