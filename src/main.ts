280ec80922a1e4c094b946378775be4b72298e7d
Outer Cloud Studio <outercloudstudio@gmail.com>
2024-03-26 00:12:31 -0500

    Update comment in main

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker.js?work
 import { createApp } from 'vue'
 import App from '@/App.vue'
 
-// WARNING TO ANYONE READING THIS: I have wasted so many hours trying to figure out why this doesn't work
-// when you using npm link to use a dev version of dash-compiler and then do npm run dev:force. It give a strange wasm error.
-// The fix is to delete the .vite folder in node-modules, then do normal npm run dev. Then it works. I have no idea why.
+// We have to do this in order to fix wasm not being intialized when testing dev builds of dash
 initRuntimes(wasmUrl)
 
 //@ts-ignore
