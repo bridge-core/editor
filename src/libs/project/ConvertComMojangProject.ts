3f6b0d6053061d12400becaa252d89b6da2bdc24
Outer Cloud <outercloudstudio@gmail.com>
2024-12-31 02:14:48 -0500

    Add converting v1 projects

--- src/libs/project/ConvertComMojangProject.ts
+++ src/libs/project/ConvertComMojangProject.ts
@@ -118,6 +118,78 @@ async function convertComMojangProject(convertableProjectInfo: ConvertableComMoj
 }
 
 async function convertV1Project(convertableProjectInfo: ConvertableV1ProjectInfo, comMojangFileSystem: BaseFileSystem) {
+	const packDefinitions: { id: string; defaultPackPath: string }[] = await Data.get('packages/minecraftBedrock/packDefinitions.json')
+	packDefinitions.push({
+		id: 'bridge',
+		defaultPackPath: '.bridge',
+	})
+
+	const projectPath = join('projects', convertableProjectInfo.name)
+
+	await fileSystem.makeDirectory(projectPath)
+
+	let manifest = null
+
+	for (const pack of convertableProjectInfo.packs) {
+		try {
+			manifest = await comMojangFileSystem.readFileJson(join(pack.path, 'manifest.json'))
+		} catch {}
+	}
+
+	let config = null
+
+	for (const pack of convertableProjectInfo.packs) {
+		try {
+			config = await comMojangFileSystem.readFileJson(join(pack.path, 'bridge', 'config.json'))
+		} catch {}
+	}
+
+	const experimentalToggles = await Data.get('packages/minecraftBedrock/experimentalGameplay.json')
+
+	packs['bridge']!.create(
+		fileSystem,
+		projectPath,
+		{
+			author: (manifest?.metadata?.authors as string[] | undefined) ?? ['bridge'],
+			bpAsRpDependency:
+				convertableProjectInfo.packs.some((pack) => pack.type === 'behaviorPack') && convertableProjectInfo.packs.some((pack) => pack.type === 'resourcePack'),
+			rpAsBpDependency:
+				convertableProjectInfo.packs.some((pack) => pack.type === 'behaviorPack') && convertableProjectInfo.packs.some((pack) => pack.type === 'resourcePack'),
+			configurableFiles: [],
+			description: manifest?.header?.description ?? '',
+			icon: convertableProjectInfo.icon,
+			name: convertableProjectInfo.name,
+			namespace: config?.prefix ?? 'bridge',
+			targetVersion: config?.formatVersion ?? (await getLatestStableFormatVersion()),
+			packs: ['bridge', ...convertableProjectInfo.packs.map((pack) => pack.type)],
+			uuids: Object.fromEntries(convertableProjectInfo.packs.map((packType) => [packType.type, uuid()])),
+			experiments: Object.fromEntries(experimentalToggles.map((toggle: any) => [toggle.id, false])),
+		},
+		join(projectPath, packDefinitions.find((pack) => pack.id === 'bridge')!.defaultPackPath)
+	)
+
+	for (const pack of convertableProjectInfo.packs) {
+		const packDefinition = packDefinitions.find((packDefinition) => packDefinition.id === pack.type)
+
+		if (!packDefinition) {
+			console.warn(`Failed to convert pack of type ${pack.type}. Could not find matching pack definition.`)
+
+			continue
+		}
+
+		await fileSystem.copyDirectoryFromFileSystem(pack.path, comMojangFileSystem, join(projectPath, packDefinition.defaultPackPath))
+	}
+
+	const projectInfo = await ProjectManager.getProjectInfo(projectPath)
+
+	if (!projectInfo) throw new Error('Failed to create project!')
+
+	for (const pack of convertableProjectInfo.packs) {
+		await comMojangFileSystem.removeDirectory(pack.path)
+	}
+
 	ProjectManager.convertableProjects.splice(ProjectManager.convertableProjects.findIndex((project) => project.packs[0].uuid === convertableProjectInfo.packs[0].uuid))
 	ProjectManager.updatedConvertableProjects.dispatch()
+
+	ProjectManager.addProject(projectInfo)
 }
