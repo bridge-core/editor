4cba6a752560b6e6ca480fa19ac766a9a796fb27
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-03-16 04:03:59 -0500

    Implement once on Event

--- src/libs/event/Event.ts
+++ src/libs/event/Event.ts
@@ -10,9 +10,41 @@ export class Event<T> {
 
 		const event = this
 
+		let disposed = false
+
 		return {
 			dispose() {
+				if (disposed) return
+
+				disposed = true
+
 				event.listeners.splice(event.listeners.indexOf(listener), 1)
+			},
+		}
+	}
+
+	public once(listener: Listener<T>): Disposable {
+		const event = this
+
+		let disposed = false
+
+		const onceListener: Listener<T> = (value?: T) => {
+			disposed = true
+
+			event.listeners.splice(event.listeners.indexOf(onceListener), 1)
+
+			listener(value)
+		}
+
+		this.listeners.push(onceListener)
+
+		return {
+			dispose() {
+				if (disposed) return
+
+				disposed = true
+
+				event.listeners.splice(event.listeners.indexOf(onceListener), 1)
 			},
 		}
 	}
