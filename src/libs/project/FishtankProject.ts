9383b08b929a103cd0a5c0b983b0dd432632b8f5
Outer Cloud <outercloudstudio@gmail.com>
2025-11-15 12:47:14 -0500

    Fix project icon

--- src/libs/project/FishtankProject.ts
+++ src/libs/project/FishtankProject.ts
@@ -1,3 +1,5 @@
+import { join } from 'pathe'
+import { fileSystem } from '../fileSystem/FileSystem'
 import { Project } from './Project'
 
 export class FishtankProject extends Project {
@@ -5,6 +7,12 @@ export class FishtankProject extends Project {
 		await super.load()
 
 		this.packs = { root: this.path }
+
+		for (const entry of await fileSystem.readDirectoryEntries(join(this.path, 'packs'))) {
+			if (!(await fileSystem.exists(join(entry.path, 'pack_icon.png')))) continue
+
+			this.icon = await fileSystem.readFileDataUrl(join(entry.path, 'pack_icon.png'))
+		}
 	}
 
 	public async dispose() {
