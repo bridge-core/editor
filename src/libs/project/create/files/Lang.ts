0d373ea9a7f52bd3f57debf073fe68eccd2f357c
Outer Cloud Studio <outercloudstudio@gmail.com>
2026-05-24 13:41:54 -0500

    fix incorrect lang entried being added when creating project
    Fixes #1620

--- src/libs/project/create/files/Lang.ts
+++ src/libs/project/create/files/Lang.ts
@@ -1,12 +1,15 @@
-import { v4 as uuid } from 'uuid'
 import { BaseFileSystem } from '@/libs/fileSystem/BaseFileSystem'
 import { join } from 'pathe'
 import { CreateProjectConfig } from '../../CreateProjectConfig'
 
-export async function createLang(fileSystem: BaseFileSystem, path: string, config: CreateProjectConfig) {
+export async function createLang(fileSystem: BaseFileSystem, path: string, config: CreateProjectConfig, packType: string) {
 	await fileSystem.makeDirectory(join(path, 'texts'))
 
-	await fileSystem.writeFile(join(path, 'texts/en_US.lang'), `skinpack.${config.namespace}=${config.name}`)
+	if (packType === 'skinPack') {
+		await fileSystem.writeFile(join(path, 'texts/en_US.lang'), `skinpack.${config.namespace}=${config.name}`)
+	} else {
+		await fileSystem.writeFile(join(path, 'texts/en_US.lang'), `pack.name=${config.name}\npack.description=${config.description}`)
+	}
 
 	await fileSystem.writeFileJson(join(path, 'texts/languages.json'), ['en_US'], true)
 }
