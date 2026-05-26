da541ec9c294ec1bdb90f78c967f2a8ea76c9e90
Outer Cloud Studio <outercloudstudio@gmail.com>
2026-05-26 07:07:00 -0500

    improve expandable component

--- src/components/Common/Expandable.vue
+++ src/components/Common/Expandable.vue
@@ -1,29 +1,5 @@
-<template>
-	<div class="bg-background-secondary overflow-hidden transition-[height] duration-100 ease-out" ref="container">
-		<div class="h-auto" ref="sizing">
-			<div
-				class="h-10 flex items-center p-3 justify-between cursor-pointer group"
-				@click="() => (expanded = !expanded)"
-			>
-				<span
-					class="group-hover:text-primary transition-colors duration-100 ease-out select-none font-theme font-medium"
-					>{{ name }}</span
-				>
-				<Icon
-					icon="arrow_drop_down"
-					class="transition-[transform, color] duration-100 ease-out group-hover:text-primary"
-					:class="{ '-rotate-180': expanded }"
-				/>
-			</div>
-			<div class="p-3 pt-1">
-				<slot />
-			</div>
-		</div>
-	</div>
-</template>
-
 <script setup lang="ts">
-import { Ref, onMounted, onUnmounted, ref, watch } from 'vue'
+import { Ref, onMounted, onUnmounted, ref } from 'vue'
 import Icon from '@/components/Common/Icon.vue'
 
 defineProps({
@@ -34,39 +10,26 @@ defineProps({
 })
 
 const expanded = ref(false)
-const container: Ref<HTMLElement | null> = ref(null)
 const sizing: Ref<HTMLElement | null> = ref(null)
 
-function updateHeight(expanded: boolean) {
-	if (!container.value) return
+const size = ref('0px')
+
+function updateSize() {
 	if (!sizing.value) return
 
-	if (expanded) {
-		container.value.style.height = `${sizing.value.scrollHeight}px`
-	} else {
-		container.value.style.height = '2.5rem'
-	}
+	size.value = `${sizing.value.getBoundingClientRect().height}px`
 }
 
-watch(expanded, updateHeight)
-
-const observer = new MutationObserver(
-	function () {
-		updateHeight(expanded.value)
-	}.bind(this)
-)
+const observer = new ResizeObserver(() => {
+	updateSize()
+})
 
 onMounted(() => {
-	updateHeight(expanded.value)
+	updateSize()
 
 	if (!sizing.value) return
 
-	observer.observe(sizing.value, {
-		childList: true,
-		subtree: true,
-		attributes: true,
-		characterData: true,
-	})
+	observer.observe(sizing.value)
 })
 
 onUnmounted(() => {
@@ -81,3 +44,26 @@ defineExpose({
 	open,
 })
 </script>
+
+<template>
+	<div
+		class="bg-background-secondary overflow-hidden transition-[height] duration-100 ease-out"
+		:style="`height: ${expanded ? size : '2.5rem'};`"
+	>
+		<div class="h-auto" ref="sizing">
+			<div class="h-10 flex items-center p-3 justify-between cursor-pointer group" @click="() => (expanded = !expanded)">
+				<span class="group-hover:text-primary transition-colors duration-100 ease-out select-none font-theme font-medium">{{
+					name
+				}}</span>
+				<Icon
+					icon="arrow_drop_down"
+					class="transition-[transform, color] duration-100 ease-out group-hover:text-primary"
+					:class="{ '-rotate-180': expanded }"
+				/>
+			</div>
+			<div class="p-3 pt-1">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
